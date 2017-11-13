1512fba5f47f4f150bc211f8476d104595a6f9ac
Mohamed Ahmed Abd Al-Fattah <mohamedstarwars@gmail.com>
2017-11-13 23:47:51 +0200

    testing

---
+++ test.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void add(int a, int b) {
+	int c = a + b;
+	printf("%d\n",c);
+}
+
+void multiply(int a,int b) {
+	int c = a * b;
+	printf("%d\n",c);
+}
+
+void viewHelp() {
+	printf("This is how to use this program.\n");
+	printf("a : add, m : multiply, h : help\n");
+	printf("e : exit program.\n");
+}
+
+int main() {
+	int a, b;
+	bool cont = true;
+	char operation;
+	while(cont) {
+		scanf("%d",&a);
+		scanf("%d",&b);
+		scanf(" %c",&operation);
+		switch(operation) {
+			case 'a':
+				add(a,b);
+				break;
+			case 'm':
+				multiply(a,b);
+				break;
+			case 'e':
+				cont = false;
+				break;
+			case 'h':
+				viewHelp();
+				break;
+			default :
+				printf("Invalid command\n");
+			}
+	}
+	return 0;
+}
