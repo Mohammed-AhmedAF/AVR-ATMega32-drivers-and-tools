1bbb0914da6e10ccc153f086dc4a7d3c111e0c24
Mohamed Ahmed <mohamedstarwars@gmail.com>
2017-12-19 13:21:33 +0200

    functionPointer added

---
+++ tests/week5/functionPointer.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int compare(int,int);
+
+int a,b;
+int res;
+
+int (*fb) (int, int);
+
+void main() {
+	printf("Enter number a:\n");
+	scanf(" %d",&a);
+	printf("Enter number b:\n");
+	scanf(" %d",&b);
+	fb = &compare;
+	res = (*fb)(a,b);
+	printf("Result is %d\n",res);
+}
+
+
+int compare(int a, int b){
+	if (a > b) {
+		return 1;
+	}
+	else if (a < b) {
+		return 2;
+	}
+	else {
+		return 0;
+	}
+}
+
