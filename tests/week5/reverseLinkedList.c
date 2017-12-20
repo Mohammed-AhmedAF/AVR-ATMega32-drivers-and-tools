836c4e5a68c70ff608a322aa6fa1739152022cfb
Mohamed Ahmed <mohamedstarwars@gmail.com>
2017-12-20 14:59:29 +0200

    reverseLinkedList added

---
+++ tests/week5/reverseLinkedList.c
@@ -0,0 +1,49 @@
+/*The aim of this program is to travers a linked list in revers*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+	int data;
+	struct node * next;
+} Node;
+
+void main() {
+	Node * head;
+	Node * current;
+	Node * previous;
+	int * dataArr = (int *) malloc(sizeof(Node)*);
+	int i;
+	/*Filling linked list*/
+	for(i = 0; i < 5; i++) {
+		if (i == 0) {
+			head = malloc(sizeof(Node));
+			head->data = i;
+			head->next = NULL;
+		}
+		else if (i == 1){
+			current = malloc(sizeof(Node));
+			current->data = i;
+			head->next = current;
+		}
+		else {
+			previous = current;
+			current = malloc(sizeof(Node));	
+			current->data = i;
+			previous->next = current;
+		}	
+	}
+	current->next = NULL;
+	/*Printing linked list contents*/
+	current = head;
+	while(current != NULL)	{
+		printf("%d", current->data);
+		dataArr= &current->data;	
+		current = current->next;
+		dataArr++;
+	}
+	/*Printing in reverse*/
+	
+}
+
+
