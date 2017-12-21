778de82176bac0ee00f28348221498244b6de044
Mohamed Ahmed <mohamedstarwars@gmail.com>
2017-12-21 09:40:00 +0200

    still trying to make reversed ll work

--- tests/week5/reverseLinkedList.c
+++ tests/week5/reverseLinkedList.c
@@ -12,10 +12,10 @@ void main() {
 	Node * head;
 	Node * current;
 	Node * previous;
-	int * dataArr = (int *) malloc(sizeof(Node)*);
-	int i;
+	int * dataArr = (int *) malloc(sizeof(Node)*5);
+	int i,val;
 	/*Filling linked list*/
-	for(i = 0; i < 5; i++) {
+	for(i = 0; i <= 8; i++) {
 		if (i == 0) {
 			head = malloc(sizeof(Node));
 			head->data = i;
@@ -31,19 +31,26 @@ void main() {
 			current = malloc(sizeof(Node));	
 			current->data = i;
 			previous->next = current;
+			current->next = NULL;
 		}	
 	}
-	current->next = NULL;
 	/*Printing linked list contents*/
 	current = head;
+	i = 0;
 	while(current != NULL)	{
 		printf("%d", current->data);
-		dataArr= &current->data;	
-		current = current->next;
+		dataArr =&current->data;
 		dataArr++;
+		current = current->next;
+		i++;
 	}
+	printf("\n");
+	printf("In reverse\n");
 	/*Printing in reverse*/
-	
+	while(i >= 0 )	{
+		printf("%d\n",*(dataArr--));
+		i--;
+	}
 }
 
 
