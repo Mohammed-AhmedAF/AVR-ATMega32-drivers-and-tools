cc45a32c57ad0b44ba88094e111977ac0b6a4b48
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-01-05 21:28:47 +0200

    seven segment

---
+++ tasks/task14/dio.h
@@ -0,0 +1,23 @@
+/*
+ * dio.h
+ *
+ *  Created on: 4 Jan 2018
+ *      Author: mohamed
+ */
+
+#ifndef DIO_H_
+#define DIO_H_
+
+typedef unsigned char u8;
+
+#define DDRA *((u8*) 0x3A)
+#define PORTA *((u8*) 0x3B)
+#define PINA *((u8*) 0x39)
+#define DDRB *((u8*) 0x37)
+#define PORTB *((u8*) 0x38)
+#define PORTC *((u8*)0x35)
+#define DDRC *((u8*)0x34)
+#define DDRD *((u8*)0x31)
+#define PORTD *((u8*)0x32)
+
+#endif /* DIO_H_ */
---
+++ tasks/task14/main.c
@@ -0,0 +1,43 @@
+/*
+ * main.c
+ *
+ *  Created on: 5 Jan 2018
+ *      Author: mohamed
+ * This code will turn on two seven-segments displays
+ * and make them display numbers from 0 to 99
+ */
+
+#include "dio.h"
+#include <util/delay.h>
+
+#define SetBit(Reg,bit) Reg |=(1<<bit)
+#define ClearBit(Reg, bit) Reg &=~(1<<bit)
+#define GetBit(Reg,bit) ((Reg>>bit)&1)
+
+u8 num[10] = {0b10100000,0b10101111,0b11000100,0b10000110,0b10001011,0b10010010,0b10010000,0b10100111,0b10000000,
+0b10000010};
+
+void main(void) {
+	u8 i, b,d;
+	DDRA = 0xff;
+	DDRB = 0xff;
+	PORTA = 0x00;
+	PORTB = 0x00;
+	DDRC = 0xff;
+	PORTC = 0x00;
+	DDRD = 0xff;
+	PORTD = 0x00;
+
+	while(1) {
+		SetBit(PORTA,0);
+		SetBit(PORTC,0);
+		for(i = 0; i < 100; i++) {
+		_delay_ms(250);
+		d = i%10;
+		b = (i-i%10)/10;
+		PORTB = num[b];
+		PORTD = num[d];
+		_delay_ms(250);
+		}
+	}
+}
