287e3b17f324d6213d5a452bc87f71912e5dee5d
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-01-10 14:08:31 +0200

    fixing dio.c

--- tests/week8/LCD/dio.c
+++ tests/week8/LCD/dio.c
@@ -55,30 +55,43 @@ u8 Dio_u8GetPortValue(u8 u8PortNumberCpy) {
 	return u8Value;
 }
 
-void Dio_u8SetPinValue(u8 u8PortNumberCpy, u8 u8PinNumberCpy,u8 u8ValueCpy) {
+void Dio_vidSetPinValue(u8 u8PortNumberCpy, u8 u8PinNumberCpy,u8 u8ValueCpy) {
 	u8 u8Port;
 	switch(u8PortNumberCpy) {
 		case DIO_PORTA:
-			u8Port = PORTA;
+			if (u8ValueCpy == 1) {
+				Set_Bit(PORTA,u8PinNumberCpy);
+			}
+			else {
+				Clear_Bit(PORTA,u8PinNumberCpy);
+			}
 			break;
 		case DIO_PORTB:
-			u8Port = PORTB;
+			if (u8ValueCpy == 1) {
+				Set_Bit(PORTB,u8PinNumberCpy);
+			}
+			else {
+				Clear_Bit(PORTB,u8PinNumberCpy);
+			}	
 			break;
 		case DIO_PORTC:
-			u8Port = PORTC;
+			if (u8ValueCpy == 1) {
+				Set_Bit(PORTC,u8PinNumberCpy);
+			}
+			else {
+				Clear_Bit(PORTC,u8PinNumberCpy);
+			}						
 			break;
 		case DIO_PORTD:
-			u8Port = PORTD;
+		if (u8ValueCpy == 1) {
+				Set_Bit(PORTD,u8PinNumberCpy);
+			}
+			else {
+				Clear_Bit(PORTD,u8PinNumberCpy);
+			}							
 			break;
 		}
-	if (u8ValueCpy == STD_HIGH) {
-		Set_Bit (u8Port,u8PinNumberCpy);
-	}
-	else {
-		Clear_Bit(u8Port,u8PinNumberCpy);
-	}
 }
-
 void Dio_u8SetPinDirection(u8 u8PortNumberCpy, u8 u8PinNumberCpy, u8 u8DirectionCpy) {
 	u8 u8Port;
 	switch(u8PortNumberCpy) {
--- tests/week8/LCD/main.c
+++ tests/week8/LCD/main.c
@@ -1,33 +1,47 @@
 #include "dio.h"
+#include <util/delay.h>
 
 void Lcd_vidInit();
-void Lcd_vidSendCommand();
-void Lcd_vidWriteCharacter();
+void Lcd_vidSendCommand(u8);
+void Lcd_vidWriteCharacter(u8);
 
 void main(void)  {
 	Dio_vidSetPortDirection(DIO_PORTD,0b11111111);
 	Dio_vidSetPortDirection(DIO_PORTA,0b00000111);
+	Dio_vidSetPortValue(DIO_PORTD,0b00000000);
 	Lcd_vidInit();
+	while(1) {
+		char name = 'M';
+		for (u8 i = 0; i < 16; i++) {
+			Lcd_vidWriteCharacter(name);	
+			Lcd_vidSendCommand(0b00010100);
+		}
+	}
 }
 
-void Lcd_vidInit() {
+void Lcd_vidInit(void) {
 	Lcd_vidSendCommand(0b00000001);
 	Lcd_vidSendCommand(0b00000010);
 	Lcd_vidSendCommand(0b00000110);
-	Lcd_vidSendCommand(0b00001110);
-	/*Function Set*/
-	Lcd_vidSendCommand(0b00111000);
+	Lcd_vidSendCommand(0b00001101);
+	Lcd_vidSendCommand(0b00010000);
 }
 
 void Lcd_vidSendCommand(u8 u8Command) {
-	Dio_vidSetPortValue(DIO_PORTA,0b00000100);
-	/*Loading command on databus*/	
+	Dio_vidSetPinValue(DIO_PORTA,0,0);
+	Dio_vidSetPinValue(DIO_PORTA,1,0);
+	Dio_vidSetPinValue(DIO_PORTA,2,1);
+	_delay_ms(50);
 	Dio_vidSetPortValue(DIO_PORTD,u8Command);
-	Dio_vidSetPortValue(DIO_PORTA,0b00000000);
-	_delay_ms(5);
-	/*Setting E to High*/
-	Dio_vidSetPortValue(DIO_PORTA,0b00000100);
+	Dio_vidSetPinValue(DIO_PORTA,2,0);
+	_delay_ms(50);
+	Dio_vidSetPinValue(DIO_PORTA,2,1);
 }
-void Lcd_vidWriteCharacter() {
-
+void Lcd_vidWriteCharacter(u8 u8DataCpy) {
+	Dio_vidSetPinValue(DIO_PORTA,0,1);
+	Dio_vidSetPinValue(DIO_PORTA,1,0);
+	Dio_vidSetPinValue(DIO_PORTA,2,1);
+	Dio_vidSetPortValue(DIO_PORTD,u8DataCpy);
+	Dio_vidSetPinValue(DIO_PORTA,2,0);
+	_delay_ms(50);
 }
