2bc82b189cd45ad58f8880ce270b7010f8e9c8cc
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-01-10 21:02:15 +0200

    Assignment done

--- tests/week8/LCD/main.c
+++ tests/week8/LCD/main.c
@@ -6,17 +6,28 @@ void Lcd_vidSendCommand(u8);
 void Lcd_vidWriteCharacter(u8);
 
 void Lcd_insertMessage(s8 *);
-u8 flag = STD_HIGH;
 u8 u8Shift = 0b00010100;
+u8 flag = 0xff;
 void main(void)  {
 	Dio_vidSetPortDirection(DIO_PORTD,0b11111111);
 	Dio_vidSetPortDirection(DIO_PORTA,0b00000111);
 	Dio_vidSetPortValue(DIO_PORTD,0b00000000);
 	Lcd_vidInit();
+	s8 * u8name = "Mohamed";
+	Lcd_insertMessage(u8name);
+
 	while(1) {
-		s8 * u8name = "Mohamed";
-		Lcd_insertMessage(u8name);
-		Lcd_vidSendCommand(0b00000001);
+		for(s8 i = 0; i < 9; i++) {
+			if (flag == 0xFF) {
+				Lcd_vidSendCommand(0x1c);
+				_delay_ms(500);
+			}
+			else {
+				Lcd_vidSendCommand(0x18);
+				_delay_ms(500);
+			}
+		}
+		flag = ~(flag);
 	}
 }
 
@@ -44,12 +55,12 @@ void Lcd_vidWriteCharacter(u8 u8DataCpy) {
 	Dio_vidSetPinValue(DIO_PORTA,2,1);
 	Dio_vidSetPortValue(DIO_PORTD,u8DataCpy);
 	Dio_vidSetPinValue(DIO_PORTA,2,0);
-	_delay_ms(50);
+	_delay_ms(500);
 }
 
 void Lcd_insertMessage(s8 * s8Message) {
 	for (s8 i = 0; i < 7; i++) {
 		Lcd_vidWriteCharacter(*s8Message++);
-		_delay_ms(1000);
 	}
+	_delay_ms(1000);
 }
