d1a60e923e5df45b317c73c78ab9138a46b72de1
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-01-10 15:47:46 +0200

    commenting init

--- tests/week8/LCD/main.c
+++ tests/week8/LCD/main.c
@@ -5,6 +5,7 @@ void Lcd_vidInit();
 void Lcd_vidSendCommand(u8);
 void Lcd_vidWriteCharacter(u8);
 u8 flag = STD_HIGH;
+u8 u8Shift = 0b00010100;
 void main(void)  {
 	Dio_vidSetPortDirection(DIO_PORTD,0b11111111);
 	Dio_vidSetPortDirection(DIO_PORTA,0b00000111);
@@ -13,18 +14,18 @@ void main(void)  {
 	while(1) {
 		char name ='M';
 		for (u8 j = 0; j < 16; j++) {
-			Lcd_vidSendCommand(0b00000001); /*Clearing LCD*/
+		Lcd_vidSendCommand(0b00000001);
 			for (u8 i = 0; i < j; i++) {
-				Lcd_vidSendCommand(0b00010100);
+				Lcd_vidSendCommand(u8Shift);
 			}
 			Lcd_vidWriteCharacter(name);
 		}
 		if (flag == STD_HIGH) {
-			Lcd_vidSendCommand(0b00010000);
+			u8Shift = 0b00010000;
 			flag = STD_LOW;
 		}
 		else  {
-			Lcd_vidSendCommand(0b00010100);
+			u8Shift = 0b00010100;
 			flag = STD_HIGH;
 		}
 	}
@@ -35,7 +36,7 @@ void Lcd_vidInit(void) {
 	Lcd_vidSendCommand(0b00000010); /*Move to home*/
 	Lcd_vidSendCommand(0b00000110); /*Set entry mode*/
 	Lcd_vidSendCommand(0b00001100); /*Display On/Off control*/
-	Lcd_vidSendCommand(0b00110000);
+	Lcd_vidSendCommand(0b00110000); /*Function Set*/
 }
 
 void Lcd_vidSendCommand(u8 u8Command) {
