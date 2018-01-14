e13009fa52a868944b56dc5488cf9feec17740a3
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-01-14 17:31:55 +0200

    include guard in lcd.h

--- tests/week9/KeyPad/lcd.h
+++ tests/week9/KeyPad/lcd.h
@@ -1,5 +1,12 @@
+#ifndef _LCD_H
+define LCD_H
+
+#define LCD_CLEAR_SCREEN 0b00000001
+#define LCD_RETURN_HOME 0b00000010
+
 void Lcd_vidSendCommand(u8);
 void Lcd_vidWriteCharacter(u8);
 void Lcd_vidGoToXY(s8,s8);
 void Lcd_vidInsertMessage(s8 *);
 
+#endif
