d06b51699500dd753a5957e3b02339fcfff44c0b
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-01-14 14:16:11 +0200

    Fully working password comparison

--- tests/week9/KeyPad/main.c
+++ tests/week9/KeyPad/main.c
@@ -8,11 +8,13 @@ void Lcd_vidGoToXY(u8,u8);
 void Lcd_vidInsertMessage(s8 *);
 void vidTakeNumber(s8);
 u8 u8Shift = 0b00010100;
+s8 * s8Message;
 s8 number = 0;
 u8 flag = 0xff;
-s8 u8KeyPad[4][4] = {{'0','c','-','='},{'1','2','3','+'},{'4','5','6','*'},{'7','8','9','/'}};
+s8 u8KeyPad[4][4] = {{'0','c','-',' '},{'1','2','3','+'},{'4','5','6','*'},{'7','8','9','/'}};
 s8 s8Password[4] = {'1','3','2','4'};
-s8 user[4];
+s8 user[16];
+s8 iUser;
 void main(void)  {
 	Dio_vidSetPortDirection(DIO_PORTD,0b11111111);
 	Dio_vidSetPortDirection(DIO_PORTA,0b00000111);
@@ -28,6 +30,7 @@ void main(void)  {
 					if(u8KeyPad[c-4][r] == 'c') {
 						Lcd_vidSendCommand(0b00000001); /*Clear screen*/
 						number = 0;
+						iUser = 0;
 						break;
 					}
 					Lcd_vidWriteCharacter(u8KeyPad[c-4][r]);	
@@ -75,21 +78,27 @@ void Lcd_vidInsertMessage(s8 * s8Message) {
 }
 
 void vidTakeNumber(s8 key) {
-	static s8 i = 0;
-	if (key == '=') {
-		for(s8 x = 0; x < 4; x++) {
-			if (s8Password[x] == user[x]) {;
-				continue;
-			}
-			if (x == 3) {
-				Lcd_vidWriteCharacter('C');
-				Lcd_vidInit();
+	if (key == ' ') {
+		if (iUser >= 5) {
+			s8Message = "Longer";
+			Lcd_vidInsertMessage(s8Message);
+			iUser = 0;
+		}
+		else {
+			for(s8 x = 0; x < 4; x++) {
+				if (s8Password[x] == user[x]) {;
+					Lcd_vidWriteCharacter('C');
+				}
+				else  {
+					Lcd_vidWriteCharacter('E');
+				}
+
 			}
 
 		}
 	}
 	else {
-		user[i] = key;
-		i++;
+		user[iUser] = key;
+		iUser++;
 	}
 }
