80f0184091413754a9bc229208de470f0b97a7bd
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-01-14 17:49:18 +0200

    decreasing delay

--- tests/week9/KeyPad/lcd.c
+++ tests/week9/KeyPad/lcd.c
@@ -4,8 +4,8 @@
 #include <util/delay.h>
 
 void Lcd_vidInit(void) {
-	Lcd_vidSendCommand(0b00000001); /*Clear screen*/
-	Lcd_vidSendCommand(0b00000010); /*Move to home*/
+	Lcd_vidSendCommand(LCD_CLEAR_SCREEN); /*Clear screen*/
+	Lcd_vidSendCommand(LCD_RETURN_HOME); /*Move to home*/
 	Lcd_vidSendCommand(0b00000110); /*Set entry mode*/
 	Lcd_vidSendCommand(0b00001100); /*Display On/Off control*/
 	Lcd_vidSendCommand(0b00111000); /*Function Set*/
@@ -28,10 +28,10 @@ void Lcd_vidSendCommand(u8 u8Command) {
 	Dio_vidSetPinValue(DIO_PORTA,0,0);
 	Dio_vidSetPinValue(DIO_PORTA,1,0);
 	Dio_vidSetPinValue(DIO_PORTA,2,1);
-	_delay_ms(50);
+	_delay_ms(3);
 	Dio_vidSetPortValue(DIO_PORTD,u8Command);
 	Dio_vidSetPinValue(DIO_PORTA,2,0);
-	_delay_ms(50);
+	_delay_ms(3);
 	Dio_vidSetPinValue(DIO_PORTA,2,1);
 }
 void Lcd_vidWriteCharacter(u8 u8DataCpy) {
@@ -44,7 +44,8 @@ void Lcd_vidWriteCharacter(u8 u8DataCpy) {
 }
 
 void Lcd_vidInsertMessage(s8 * s8Message) {
-	for (s8 i = 0; i < 6; i++) {
+	Lcd_vidSendCommand(LCD_CLEAR_SCREEN);
+	while (*s8Message != '\0') {
 		Lcd_vidWriteCharacter(*s8Message++);
 	}
 	_delay_ms(1000);
