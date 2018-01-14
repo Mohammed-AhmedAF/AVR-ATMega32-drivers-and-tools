ba96622b70befa96efca6d342ebff490c1c4c7da
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-01-14 13:21:14 +0200

    transistor project

--- tests/week8/trans/main.c
+++ tests/week8/trans/main.c
@@ -2,11 +2,13 @@
 #include <util/delay.h>
 void main(void) {
 	Dio_vidSetPortDirection(DIO_PORTA,0b00000011);
-	Dio_vidSetPortValue(DIO_PORTA,0b00000011);
+	Dio_vidSetPortValue(DIO_PORTA,0b00000010);
 	while(1) {
-		Dio_vidSetPinValue(DIO_PORTA,1,1);
-		_delay_ms(500);
+		Dio_vidSetPinValue(DIO_PORTA,0,1);
 		Dio_vidSetPinValue(DIO_PORTA,1,0);
-		_delay_ms(500);
+		_delay_ms(1000);
+		Dio_vidSetPinValue(DIO_PORTA,0,0);
+		Dio_vidSetPinValue(DIO_PORTA,1,1);
+		_delay_ms(1000);
 	}
 }
