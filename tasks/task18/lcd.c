0f1b91bdbcdd96af37a7ba26c815105264b32818
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-01-20 11:31:39 +0200

    interrupt with lcd

---
+++ tasks/task18/lcd.c
@@ -0,0 +1,74 @@
+#include "Std_Types.h"
+#include "lcd.h"
+#include "dio.h"
+#include <util/delay.h>
+
+void Lcd_vidInit(void) {
+	Lcd_vidSendCommand(LCD_CLEAR_SCREEN); /*Clear screen*/
+	Lcd_vidSendCommand(LCD_RETURN_HOME); /*Move to home*/
+	Lcd_vidSendCommand(0b00000110); /*Set entry mode*/
+	Lcd_vidSendCommand(0b00001100); /*Display On/Off control*/
+	Lcd_vidSendCommand(0b00111000); /*Function Set*/
+}
+
+void Lcd_vidGoToXY(s8 s8xCpy, s8 s8yCpy) {
+	Lcd_vidSendCommand(LCD_RETURN_HOME);
+	if (s8yCpy == 1) {
+		for (s8 i = 0; i < s8xCpy;i++) {
+			Lcd_vidSendCommand(0b00010100);
+		}	
+	}
+	else {
+		for (s8 i = 0; i < s8xCpy+40; i++) {
+			Lcd_vidSendCommand(0b00010100);
+		}
+	}
+}
+
+void Lcd_vidSendCommand(u8 u8Command) {
+	Dio_vidSetPinValue(DIO_PORTA,0,0);
+	Dio_vidSetPinValue(DIO_PORTA,1,0);
+	Dio_vidSetPinValue(DIO_PORTA,2,1);
+	Dio_vidSetPortValue(DIO_PORTD,u8Command);
+	_delay_ms(1);
+	Dio_vidSetPinValue(DIO_PORTA,2,0);
+	_delay_ms(1);
+	Dio_vidSetPinValue(DIO_PORTA,2,1);
+}
+void Lcd_vidWriteCharacter(u8 u8DataCpy) {
+	Dio_vidSetPinValue(DIO_PORTA,0,1);
+	Dio_vidSetPinValue(DIO_PORTA,1,0);
+	Dio_vidSetPinValue(DIO_PORTA,2,1);
+	Dio_vidSetPortValue(DIO_PORTD,u8DataCpy);
+	Dio_vidSetPinValue(DIO_PORTA,2,0);
+	_delay_ms(5);
+}
+
+void Lcd_vidInsertMessage(s8 * s8Message) {
+	while (*s8Message != '\0') {
+		Lcd_vidWriteCharacter(*s8Message++);
+	}
+}
+
+void Lcd_vidInsertSizedMessage(s8 * s8Message,u8 size) {
+	while (size > 0) {
+		Lcd_vidWriteCharacter(*s8Message++);
+		size = size - 1;
+	}
+}
+
+void Lcd_vidBlinkMessage(s8 * s8MessageCpy,s8 s8nCpy) {
+	s8 * s8Message = s8MessageCpy;
+	while (s8nCpy > 0) {
+		Lcd_vidSendCommand(LCD_CLEAR_SCREEN);
+		Lcd_vidSendCommand(LCD_RETURN_HOME);
+		_delay_ms(100);
+		while (*s8Message != '\0') {
+			Lcd_vidWriteCharacter(*s8Message++);
+		}
+		s8Message = s8MessageCpy;
+		s8nCpy--;
+		_delay_ms(1000);
+	}
+}
+
---
+++ tasks/task18/lcd.h
@@ -0,0 +1,16 @@
+#ifndef _LCD_H
+#define _LCD_H
+
+#define LCD_CLEAR_SCREEN 0b00000001
+#define LCD_RETURN_HOME 0b00000010
+#include "Std_Types.h"
+
+void Lcd_vidInit(void);
+void Lcd_vidInsertSizedMessage(s8 *, u8);
+void Lcd_vidSendCommand(u8);
+void Lcd_vidWriteCharacter(u8);
+void Lcd_vidGoToXY(s8,s8);
+void Lcd_vidInsertMessage(s8 *);
+void Lcd_vidBlinkMessage(s8 *, s8);
+
+#endif
