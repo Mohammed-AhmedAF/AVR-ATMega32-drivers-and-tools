bd69db8863973581dd0b88de19b711151b5f15dd
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-01-22 14:07:00 +0200

    practicing

---
+++ tests/week10/timerAgain/main.c
@@ -0,0 +1,56 @@
+#include "dio.h"
+#include "lcd.h"
+#include "Std_Types.h"
+#include "Macros.h"
+#include <util/delay.h>
+#include <avr/interrupt.h>
+volatile u8 count;
+
+void main(void) {
+
+	Dio_vidSetPortDirection(DIO_PORTA,0b10000111);
+	Dio_vidSetPortDirection(DIO_PORTA,0b11111111);
+	Lcd_vidInit();
+
+	Set_Bit(TIMSK,0);
+
+	//Setting prescaler
+	Set_Bit(TCCR0,0);
+	Clear_Bit(TCCR0,1);
+	Set_Bit(TCCR0,2);
+
+	Set_Bit(SREG,7);
+
+	while(1) {
+
+		if (count == 30)  {
+			Toggle_Bit(PORTA,7);
+			count = 0;
+		}
+	}
+}
+
+void Lcd_vidShowCount(u8 res) {
+	u8 * message;
+	u8 arr[3];
+	u8 u8a;
+	u8 x = 100;
+	for (s8 i = 0; i < 3; i++) {
+		u8a = (res/x)+48;
+		res = res - (res/x)*x;
+		arr[i] = u8a;
+		x = x/10;
+	}
+	message = arr;
+	Lcd_vidInsertSizedMessage(message,3);
+	Lcd_vidSendCommand(LCD_CLEAR_SCREEN);
+	Lcd_vidSendCommand(LCD_RETURN_HOME);
+
+}
+
+ISR(TIMER0_OVF_vect) {
+	count++;
+	Lcd_vidShowCount(TCNT0);
+}
+
+
