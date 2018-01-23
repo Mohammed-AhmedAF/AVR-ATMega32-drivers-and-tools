6455c48b9bf746906b85e573a8f99a59d8b809a1
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-01-23 16:25:04 +0200

    just redoing the main file

--- tests/week10/timer3/main.c
+++ tests/week10/timer3/main.c
@@ -1,36 +1,33 @@
 #include "dio.h"
-#include "Std_Types.h"
 #include "Macros.h"
+#include "Std_Types.h"
 #include <avr/interrupt.h>
 
 volatile u8 count = 0;
 
 void main(void) {
+	Dio_vidSetPortDirection(DIO_PORTA,0B10000000);
+	
 
-	Dio_vidSetPortDirection(DIO_PORTA,0b10000001);
-
-
-	//enabling global interrupt
+	//Setting global interrupt
 	Set_Bit(SREG,7);
 
-	//Setting prescaler
-	Set_Bit(TCCR0,0);
-	Clear_Bit(TCCR0,1);
+	//Setting prescaler in TCCR0
 	Set_Bit(TCCR0,2);
+	Clear_Bit(TCCR0,1);
+	Set_Bit(TCCR0,0);
 
-	//Set Timer Enable in Timer/Counter Interrupt Mask Register
+	//Setting Timer Interrupt Mask in TIMSK
 	Set_Bit(TIMSK,0);
-
 
 	while(1) {
 		if (count == 30) {
-			if (TCNT0 == 170) {
-				Toggle_Bit(PORTA,7);
-				count = 0;
-			}
+			Toggle_Bit(PORTA,7);
+			count = 0;
 		}
 	}
 
+	
 }
 
 ISR (TIMER0_OVF_vect) {
