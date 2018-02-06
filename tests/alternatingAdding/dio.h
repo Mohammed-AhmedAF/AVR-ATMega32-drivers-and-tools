21edc4b334b6fc9461e69c79f8b5c4619211f82e
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-02-06 12:03:45 +0200

    moving project to tests

---
+++ tests/alternatingAdding/dio.h
@@ -0,0 +1,19 @@
+/*
+ * dio.h
+ *
+ *  Created on: 31 Dec 2017
+ *      Author: mohamed
+ */
+
+
+#ifndef DIO_H_
+#define DIO_H_
+
+#include "types.h"
+
+#define DDRA  *((u8*)0x3A)
+#define PORTA  *((u8*)0x3B)
+
+
+
+#endif /* DIO_H_ */
---
+++ tests/alternatingAdding/types.h
@@ -0,0 +1,16 @@
+/*
+ * types.h
+ *
+ *  Created on: 31 Dec 2017
+ *      Author: mohamed
+ */
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+typedef unsigned char u8;
+typedef unsigned int u32;
+typedef int s32;
+
+
+#endif /* TYPES_H_ */
