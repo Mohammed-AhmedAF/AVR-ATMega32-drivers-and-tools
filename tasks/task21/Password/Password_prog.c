9eb23ec03587d9882ccdaf31aeec33064444de98
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-02-12 14:13:16 +0200

    Record will show id and password precisely

--- tasks/task21/Password/Password_prog.c
+++ tasks/task21/Password/Password_prog.c
@@ -9,14 +9,16 @@
 #include "Services_interface.h"
 #include <util/delay.h>
 
-void Password_vidSavePassword(u8 * u8idCpy, u8 * u8passwordCpy) {
+void Password_vidSavePassword(u8 * u8idCpy, u8 * u8passwordCpy,u8 u8idSize,u8 u8passwordSize) {
 	u8 i = 0;
 	/*saving id*/
-	for (i = 0; i < 5; i++) {
+	db[u8userIndex].u8idSize = u8idSize;
+	db[u8userIndex].u8passwordSize = u8passwordSize;
+	for (i = 0; i < u8idSize; i++) {
 		db[u8userIndex].u8id[i] = *u8idCpy++;
 	}
 	/*saving password*/
-	for (i = 0; i < 6; i++) {
+	for (i = 0; i < u8passwordSize; i++) {
 		db[u8userIndex].u8password[i] = *u8passwordCpy++;
 	}
 	u8userIndex++;
@@ -33,6 +35,7 @@ void Password_vidAskID(void) {
 			i++;
 		}
 	}while(u8keyPressed != '#');
+	u8idSize = i;
 }
 
 void Password_vidAskPassword(void) {
@@ -46,6 +49,7 @@ void Password_vidAskPassword(void) {
 			i++;
 		}
 	}while(u8keyPressed != '#');
+	u8passwordSize = i;
 }
 
 void Password_vidAskReenter(void) {
@@ -68,13 +72,13 @@ void Password_vidViewRecord(void) {
 		LCD_vidSendCommand(LCD_CLEAR_SCREEN);
 		/*Viewing ID*/
 		LCD_vidWriteString(ID);
-		for (i = 0; i < 5; i++) {
+		for (i = 0; i < db[x].u8idSize; i++) {
 			LCD_vidWriteCharacter(db[x].u8id[i]);
 		}
 		/*Viewing password*/
 		LCD_vidGoToXY(0,2);
 		LCD_vidWriteString(PASSWORD);
-		for (i = 0; i < 6; i++) {
+		for (i = 0; i < db[x].u8passwordSize; i++) {
 			LCD_vidWriteCharacter(db[x].u8password[i]);
 		}
 		_delay_ms(1000);
@@ -93,7 +97,7 @@ void Password_vidCheckMatch(void) {
 			LCD_vidSendCommand(LCD_CLEAR_SCREEN);
 			LCD_vidWriteString(PASSWORD_SAVED);
 			_delay_ms(500);
-			Password_vidSavePassword(u8id,u8passwordTemp);
+			Password_vidSavePassword(u8id,u8passwordTemp,u8idSize,u8passwordSize);
 			break;
 		}
 	}
