78eb3080d09a371f816f9ae8eb1ffa1a1322db64
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-02-16 11:37:25 +0200

    project complete

--- tests/week14/Ext/main.c
+++ tests/week14/Ext/main.c
@@ -2,14 +2,19 @@
 #include "Std_Types.h"
 #include "DIO_interface.h"
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+u8 u8reading;
+
 void main(void) {
 	
-
+	DIO_vidSetPinDirection(DIO_PORTB,0,1);
+	DIO_vidSetPinDirection(DIO_PORTB,1,1);
 	/*Configuring global interrupt*/
-	SET_BIT(SREG,7)
+	SET_BIT(SREG,7);
 
-	DIO_vidPinDirection(DIO_PORTD,2,0);
-	DIO_vidPinValue(DIO_PORTD,2,1);
+	DIO_vidSetPinDirection(DIO_PORTD,2,0);
+	DIO_vidSetPinValue(DIO_PORTD,2,1);
 
 	/*MCUCR : Interrupt Sense Control*/
 	SET_BIT(MCUCR,1);
@@ -26,11 +31,11 @@ void main(void) {
 	SET_BIT(ADMUX,5);
 
 	/*Selecting channel*/
-	SET_BIT(ADMUX,4);
-	SET_BIT(ADMUX,3);
-	SET_BIT(ADMUX,2);
-	SET_BIT(ADMUX,1);
-	SET_BIT(ADMUX,0);
+	CLEAR_BIT(ADMUX,4);
+	CLEAR_BIT(ADMUX,3);
+	CLEAR_BIT(ADMUX,2);
+	CLEAR_BIT(ADMUX,1);
+	CLEAR_BIT(ADMUX,0);
 
 	/*ADCSRA*/
 	SET_BIT(ADCSRA,7);
@@ -47,6 +52,25 @@ void main(void) {
 	SET_BIT(SFIOR,6);
 	CLEAR_BIT(SFIOR,5);
 
+	u8 u8analog;
+	while(1) {
+		u8analog = (u8reading*5)/256;
+		if (u8analog > 2) {
+			DIO_vidSetPinValue(DIO_PORTB,1,1);	
+			DIO_vidSetPinValue(DIO_PORTB,0,0);
+		}
+		else {
+			DIO_vidSetPinValue(DIO_PORTB,0,1);
+			DIO_vidSetPinValue(DIO_PORTB,1,0);
+		}
+	}
+
 }
 
+ISR(INT0_vect) {
 
+}
+
+ISR(ADC_vect) {
+	u8reading = ADCH;
+}
