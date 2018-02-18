22283538b90c9fc787cc98bbf7a6cafef00b405c
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-02-18 08:03:31 +0200

    adding PIN macros

--- lib/DIO_interface.h
+++ lib/DIO_interface.h
@@ -9,6 +9,15 @@
 #define DIO_PORTC 2
 #define DIO_PORTD 3
 
+#define DIO_PIN0 0
+#define DIO_PIN1 1
+#define DIO_PIN2 2
+#define DIO_PIN3 3
+#define DIO_PIN4 4
+#define DIO_PIN5 5
+#define DIO_PIN6 6
+#define DIO_PIN7 7
+
 #define DDRA *((u8*) 0x3A)
 #define PORTA *((u8*) 0x3B)
 #define PINA *((u8*) 0x39)
