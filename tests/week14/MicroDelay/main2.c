7e3f72ae3d7edf06043ebd24d14bdfb836160e11
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-02-22 11:26:10 +0200

    checking in ISR

--- tests/week14/MicroDelay/main2.c
+++ tests/week14/MicroDelay/main2.c
@@ -4,6 +4,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+
 volatile u32 u32time_var = 0;
 volatile u32 u32desiredTime = 0;
 volatile u8 u8endFlag = 0;
@@ -11,7 +12,7 @@ void Timer_vidDelayMicroSec(u32);
 void main(void) {
 
 	DIO_vidSetPinDirection(DIO_PORTA,DIO_PIN0,1);
-
+	DIO_vidSetPinValue(DIO_PORTA,DIO_PIN0,0);
 	SET_BIT(TCCR0,0);
 	CLEAR_BIT(TCCR0,1);
 	CLEAR_BIT(TCCR0,2);
@@ -34,17 +35,16 @@ void main(void) {
 }
 
 void Timer_vidDelayMicroSec(u32 u32timeCpy) {
-	u32desiredTime = 0;
-
 	u32desiredTime = (u32) (u32timeCpy);
 	while (u8endFlag == 0) {
-		if(u32time_var == u32desiredTime) {
-			u8endFlag = 1;
-			u32time_var = 0;
-		}
 	}
+	u8endFlag = 0;
 }
 
 ISR(TIMER0_COMP_vect) {
 	u32time_var++;
+	if(u32time_var == u32desiredTime) {
+		u8endFlag = 1;
+		u32time_var = 0;
+	}
 }
