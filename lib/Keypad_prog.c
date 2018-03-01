2459734e26302fcafc10abc6d8b9426b4a5bb336
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-03-01 11:54:00 +0200

    Making the keypad module shorter

--- lib/Keypad_prog.c
+++ lib/Keypad_prog.c
@@ -21,96 +21,34 @@ void KEYPAD_vidInit() {
 
 }
 
+
+u8 keys[4][4] = {{'/',3,2,1},{'*',6,5,4},{'+',3,2,1},{'=','-',0,'#'}};
+u8 values[4] = {0x0E,0x0D,0x0B,0x07};
+u8 value,i = 0;
 u8 KEYPAD_u8GetKey(void) {
 	/*1st*/
-	u8 x = 0;
-	DIO_vidSetPinValue(ROW0_PORT,ROW0_PIN,0);
-	DIO_vidSetPinValue(ROW1_PORT,ROW1_PIN,1);
-	DIO_vidSetPinValue(ROW2_PORT,ROW2_PIN,1);
-	DIO_vidSetPinValue(ROW3_PORT,ROW3_PIN,1);
+	for (i = 0; i < 4; i++) {
+		u8 x = 0;
+		value = values[i];
+		DIO_vidSetPinValue(ROW0_PORT,ROW0_PIN,GET_BIT(value,0));
+		DIO_vidSetPinValue(ROW1_PORT,ROW1_PIN,GET_BIT(value,1));
+		DIO_vidSetPinValue(ROW2_PORT,ROW2_PIN,GET_BIT(value,2));
+		DIO_vidSetPinValue(ROW3_PORT,ROW3_PIN,GET_BIT(value,3));
 
-	x |= (DIO_u8GetPinValue(COL0_PORT,COL0_PIN) << 0);
-	x |= (DIO_u8GetPinValue(COL1_PORT,COL1_PIN) << 1);
-	x |= (DIO_u8GetPinValue(COL2_PORT,COL2_PIN) << 2);
-	x |= (DIO_u8GetPinValue(COL3_PORT,COL3_PIN) << 3);
-	switch(x) {
-		case 0x07:
-			return '/';
-		case 0x0B:
-			return 3;
-		case 0x0D:
-			return 2;
-		case 0x0E:
-			return 1;
-
-	}	
-	/*2nd*/
-	x = 0;
-	DIO_vidSetPinValue(ROW0_PORT,ROW0_PIN,1);
-	DIO_vidSetPinValue(ROW1_PORT,ROW1_PIN,0);
-	DIO_vidSetPinValue(ROW2_PORT,ROW2_PIN,1);
-	DIO_vidSetPinValue(ROW3_PORT,ROW3_PIN,1);
-
-	x |= (DIO_u8GetPinValue(COL0_PORT,COL0_PIN) << 0);
-	x |= (DIO_u8GetPinValue(COL1_PORT,COL1_PIN) << 1);
-	x |= (DIO_u8GetPinValue(COL2_PORT,COL2_PIN) << 2);
-	x |= (DIO_u8GetPinValue(COL3_PORT,COL3_PIN) << 3);
-	switch(x) {
-		case 0x07:
-			return '*';
-		case 0x0B:
-			return 6;
-		case 0x0D:
-			return 5;
-		case 0x0E:
-			return 4;
-	}
-
-	/*3d*/
-	x =0;
-	DIO_vidSetPinValue(ROW0_PORT,ROW0_PIN,1);
-	DIO_vidSetPinValue(ROW1_PORT,ROW1_PIN,1);
-	DIO_vidSetPinValue(ROW2_PORT,ROW2_PIN,0);
-	DIO_vidSetPinValue(ROW3_PORT,ROW3_PIN,1);
-
-	x |= (DIO_u8GetPinValue(COL0_PORT,COL0_PIN) << 0);
-	x |= (DIO_u8GetPinValue(COL1_PORT,COL1_PIN) << 1);
-	x |= (DIO_u8GetPinValue(COL2_PORT,COL2_PIN) << 2);
-	x |= (DIO_u8GetPinValue(COL3_PORT,COL3_PIN) << 3);
-
-	switch(x) {
-		case 0x07:
-			return '+';
-		case 0x0B:
-			return 9;
-		case 0x0D:
-			return 8;
-		case 0x0E:
-			return 7;
-	}
-
-	/*4th*/
-	x = 0;
-	DIO_vidSetPinValue(ROW0_PORT,ROW0_PIN,1);
-	DIO_vidSetPinValue(ROW1_PORT,ROW1_PIN,1);
-	DIO_vidSetPinValue(ROW2_PORT,ROW2_PIN,1);
-	DIO_vidSetPinValue(ROW3_PORT,ROW3_PIN,0);
-	
-
-	x |= (DIO_u8GetPinValue(COL0_PORT,COL0_PIN) << 0);
-	x |= (DIO_u8GetPinValue(COL1_PORT,COL1_PIN) << 1);
-	x |= (DIO_u8GetPinValue(COL2_PORT,COL2_PIN) << 2);
-	x |= (DIO_u8GetPinValue(COL3_PORT,COL3_PIN) << 3);
-
-	switch(x) {
-		case 0x07:
-			return '-';
-		case 0x0B:
-			return '=';
-		case 0x0D:
-			return 0;
-		case 0x0E:
-			return '#';
+		x |= (DIO_u8GetPinValue(COL0_PORT,COL0_PIN) << 0);
+		x |= (DIO_u8GetPinValue(COL1_PORT,COL1_PIN) << 1);
+		x |= (DIO_u8GetPinValue(COL2_PORT,COL2_PIN) << 2);
+		x |= (DIO_u8GetPinValue(COL3_PORT,COL3_PIN) << 3);
+		switch(x) {
+			case 0x07:
+				return keys[i][0];
+			case 0x0B:
+				return keys[i][1];
+			case 0x0D:
+				return keys[i][2];
+			case 0x0E:
+				return keys[i][3];
+		}	
 	}
 	return 0xFF;
 }
