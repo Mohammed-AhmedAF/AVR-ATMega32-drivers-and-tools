c5254ea571b1b9d508a6dbba8500634e8ee77428
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-03-03 03:58:18 +0200

    Removing unused files

--- tests/week16/UART/SevenSegment_interface.h
+++
@@ -1,7 +0,0 @@
-#ifndef SEVENSEGMENT_INTERFACE.H
-#define SEVENSEGMENT_INTERFACE.H 
-
-void SevenSegment_vidInit(u8,u8);
-void  SevenSegment_vidWriteNumber(u8);
-#endif
-
--- tests/week16/UART/StepperMotor_interface.h
+++
@@ -1,5 +0,0 @@
-#ifndef STEPPERMOTOR_INTERFACE.H
-#define STEPPERMOTOR_INTERFACE.H
-void StepperMotor_vidInit(void);
-void StepperMotor_vidMove(void);
-#endif
