4c0be5f9bc79a636a1885353926bab826a7a8792
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-03-13 12:56:17 +0200

    Deleting unused files

--- tests/week17/Arduino/Services_interface.h
+++
@@ -1,5 +0,0 @@
-#ifndef SERVICES_INTERFACE_H
-#define SERVICES_INTERFACE_H
-void Services_vidWriteCharacter(void);
-u8 u8keyPressed;
-#endif
--- tests/week17/Arduino/UART_interface.h
+++
@@ -1,8 +0,0 @@
-#ifndef UART_INTERFACE
-#define UART_INTERFACE
-void UART_vidInit(void);
-void UART_vidSendByte(u8);
-u8 UART_u8ReceiveByte(void);
-void UART_vidSendLine(void);
-void UART_vidSendString(u8 *);
-#endif
