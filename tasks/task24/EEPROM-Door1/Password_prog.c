0967af6f0f798dc39fbb9ffaa7203caec6c85bcd
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-03-15 11:30:25 +0200

    Made some functions

--- tasks/task24/EEPROM-Door1/Password_prog.c
+++ tasks/task24/EEPROM-Door1/Password_prog.c
@@ -8,9 +8,14 @@
 
 u8 i;
 u8 id[9];
+u8 pass[9];
+u8 passR[9];
 u8 u8Byte;
 u8 flag = 0;
-u8 u8IDSize = 0;;
+u8 u8IDSize = 0;
+u8 u8PasswordSize;
+u8 u8PasswordStart;
+
 void Password_vidAskID(void) {
 	LCD_vidSendCommand(LCD_CLEAR_SCREEN);
 	LCD_vidWriteString("ID: ");
@@ -25,9 +30,10 @@ void Password_vidAskID(void) {
 			i++;
 			u8IDSize++;
 		}
-	
+
 	}while(1);
 }
+
 
 void Password_vidShowID(u8 u8IDSize) {
 	LCD_vidSendCommand(LCD_CLEAR_SCREEN);
@@ -42,18 +48,83 @@ void Password_vidShowID(u8 u8IDSize) {
 void Password_vidSaveID(u8 u8IDSize) {
 	u8 c;
 	u8 flag;
+	u8PasswordStart = u8IDSize-1;
+	LCD_vidSendCommand(LCD_CLEAR_SCREEN);
 	for (i = 0; i < u8IDSize; i++) {
 		c = id[i];
-		flag = EEPROM_u8WriteByte(i,c);	
+		flag = EEPROM_u8WriteByte(i,c);
 		if (flag == 1) {
-			LCD_vidWriteCharacter('$');
-		}
-		else {
 			LCD_vidWriteCharacter('!');
 		}
+		else {
+			LCD_vidWriteCharacter('/');
+		}
 	}
+	LCD_vidWriteString("ID saved");
+}
+
+void Password_vidReenterPassword(void) {
+	LCD_vidSendCommand(LCD_CLEAR_SCREEN);
+	LCD_vidWriteString("Reenter pass: ");
 	LCD_vidGoToXY(0,2);
-	LCD_vidWriteString("ID saved!");
+	i = 0;
+	do {
+		Services_vidWriteCharacter();
+		if(u8keyPressed == '#') {
+			break;
+		}
+		else {
+			passR[i] = u8keyPressed;
+			i++;
+		}
+	}while(1);
+}
+
+void Password_vidSavePassword(u8 u8PasswordStart,u8 u8PasswordSize) {
+	u8 flag;
+	u8 c;
+	u8 u8PasswordIndex = u8PasswordStart;
+	LCD_vidGoToXY(0,2);
+	for (i = 0; i < u8PasswordSize; i++) {
+		c = pass[i];
+		flag = EEPROM_u8WriteByte(u8PasswordIndex++,c);
+	}
+
+	LCD_vidWriteString("Password saved");
+
+}
+
+void Password_vidAskPassword(void) {
+	LCD_vidSendCommand(LCD_CLEAR_SCREEN);
+	LCD_vidWriteString("Pass: ");
+	LCD_vidGoToXY(0,2);
+	i = 0;
+	do {
+		Services_vidWriteCharacter();
+		if (u8keyPressed == '#') {
+			break;
+		}
+		else {
+			pass[i] = u8keyPressed;
+			i++;
+			u8PasswordSize++;
+		}
+
+	}while(1);
+
+}
+
+u8 Password_vidCheckPasswordMatch(u8 u8PasswordSizeCpy) {
+	for (i = 0; i < u8PasswordSizeCpy; i++) {
+		if (pass[i] != passR[i]) {
+			LCD_vidSendCommand(LCD_CLEAR_SCREEN);
+			LCD_vidWriteString("No match!");
+			return 0;
+		}	
+		else if(i == (u8PasswordSizeCpy-1)) {
+			return 1;
+		}
+	}
 }
 
 void Password_vidRetreiveID(u8 u8IDSize) {
@@ -67,6 +138,23 @@ void Password_vidRetreiveID(u8 u8IDSize) {
 		}
 		else {
 			LCD_vidWriteCharacter('?');
+		}
+	}
+}
+
+void Password_vidReadPassword(u8 u8PasswordStartCpy, u8 u8PasswordSizeCpy) {
+	u8 u8PasswordIndex;
+	u8PasswordIndex = u8PasswordStartCpy;
+	LCD_vidSendCommand(LCD_CLEAR_SCREEN);
+	LCD_vidWriteString("Password is: ");
+	for (i = 0; i < u8PasswordSizeCpy; i++) {
+		flag = EEPROM_u8ReadByte(u8PasswordIndex,&u8Byte);		
+		u8PasswordIndex++;
+		if (flag == 1) {
+			LCD_vidWriteCharacter(u8Byte);
+		}
+		else {
+			LCD_vidWriteCharacter(u8PasswordIndex+'0');
 		}
 	}
 }
--- tasks/task24/EEPROM-Door1/main.c
+++ tasks/task24/EEPROM-Door1/main.c
@@ -9,15 +9,27 @@
 #include "EEPROM_interface.h"
 #include <util/delay.h>
 
+u8 u8Repeat,u8MatchFlag;
 extern u8 u8IDSize;
+extern u8 u8PasswordSize;
+extern u8 u8PasswordStart;
+
 int main(void) {
 	LCD_vidInit();
 	KEYPAD_vidInit();	
-	
+	EEPROM_vidInit();
+
 	Password_vidAskID();
-	Password_vidShowID(u8IDSize);
+	Password_vidAskPassword();
+	Password_vidReenterPassword();
+	do {
+	u8MatchFlag = Password_vidCheckPasswordMatch(u8PasswordSize);
+	}while(u8MatchFlag == 0);
+
 	Password_vidSaveID(u8IDSize);
+	Password_vidSavePassword(u8PasswordStart,u8PasswordSize);
 	_delay_ms(2000);
 	Password_vidRetreiveID(u8IDSize);
+	//Password_vidReadPassword(u8PasswordStart,u8PasswordSize);
 	while(1);
 }
