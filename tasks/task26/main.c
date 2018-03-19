24b2afdd3b028cb7b0fe843ce8bcff9535d92b67
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-03-19 22:19:30 +0200

    Adding second LED function

--- tasks/task26/main.c
+++ tasks/task26/main.c
@@ -6,17 +6,30 @@
 #include <util/delay.h>
 
 void vidCount(void);
-void vidToggleLED(void);
+void vidToggleLED1(void);
+void vidToggleLED2(void);
+void vidCheckButton(void);
 
 Task_Type Task1;
 Task_Type Task2;
+Task_Type Task3;
+Task_Type Task4;
 
 u8 i;
 void main(void) {
 	//Initialization
-	SevenSegment_vidInit(DIO_PORTB,DIO_PORTC);
+
 	DIO_vidSetPinDirection(DIO_PORTA,DIO_PIN0,STD_HIGH);
+	DIO_vidSetPinDirection(DIO_PORTA,DIO_PIN1,STD_HIGH);
+
+	DIO_vidSetPinDirection(DIO_PORTA,DIO_PIN2,STD_LOW);
+	DIO_vidSetPinValue(DIO_PORTA,DIO_PIN2,STD_HIGH);
+
+
+	SevenSegment_vidInit(DIO_PORTB,DIO_PORTC);
 	RTO_vidInit();
+
+
 	Task1.periodicity = 500;
 	Task1.first_delay = 0;
 	Task1.state = RTO_U8TASK_RUNNING;
@@ -24,11 +37,24 @@ void main(void) {
 
 	Task2.periodicity = 500;
 	Task2.first_delay = 0;
-	Task2.state = RTO_U8TASK_RUNNING;
-	Task2.ptrfun = vidToggleLED;
+	Task2.state = RTO_U8TASK_PAUSED;
+	Task2.ptrfun = vidToggleLED1;
+
+	Task3.periodicity = 500;
+	Task3.first_delay = 0;
+	Task3.state = RTO_U8TASK_PAUSED;
+	Task3.ptrfun = vidToggleLED2;
+
+	Task4.periodicity = 500;
+	Task4.first_delay = 0;
+	Task4.state = RTO_U8TASK_RUNNING;
+	Task4.ptrfun = vidCheckButton;
+
 
 	RTO_vidCreateTask(&Task1,1);
 	RTO_vidCreateTask(&Task2,2);
+	RTO_vidCreateTask(&Task3,3);
+	RTO_vidCreateTask(&Task4,4);
 	while(1);
 	
 }
@@ -42,6 +68,21 @@ void vidCount(void) {
 	}
 }
 
-void vidToggleLED(void) {
+void vidToggleLED1(void) {
 	TOGGLE_BIT(PORTA,0);
+}
+
+void vidCheckButton(void) {
+	if (DIO_u8GetPinValue(DIO_PORTA,DIO_PIN2) == 1) {
+		Task3.state = RTO_U8TASK_RUNNING;
+		Task2.state = RTO_U8TASK_PAUSED;
+	}
+	else {
+		Task3.state = RTO_U8TASK_PAUSED;
+		Task2.state = RTO_U8TASK_RUNNING;
+	}
+}
+
+void vidToggleLED2(void) {
+	TOGGLE_BIT(PORTA,1);
 }
