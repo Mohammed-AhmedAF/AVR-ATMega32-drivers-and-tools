5dbf474e8c2fc1a6206e3a4cf361b8797d9c6b50
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-03-20 17:33:09 +0200

    Fixing intensity functions

--- tasks/task26/main.c
+++ tasks/task26/main.c
@@ -3,6 +3,7 @@
 #include "DIO_interface.h"
 #include "RTO_interface.h"
 #include "SevenSegment_interface.h"
+#include "LCD_interface.h"
 #include <util/delay.h>
 #include <avr/io.h>
 
@@ -13,36 +14,39 @@ void vidToggleLED2(void);
 void vidCheckButton(void);
 void vidDecreaseIntensity(void);
 void vidIncreaseIntensity(void);
+void vidDisplayIntensity(void);
 Task_Type Task1;
 Task_Type Task2;
 Task_Type Task3;
 Task_Type Task4;
 Task_Type Task5;
 Task_Type Task6;
+Task_Type Task7;
+
 
 u8 i;
-u8 u8Intensity = 0;
+s16 s16Intensity = 0;
 void main(void) {
 	//Initialization
 
-	DIO_vidSetPinDirection(DIO_PORTA,DIO_PIN0,STD_HIGH); //LED1
-	DIO_vidSetPinDirection(DIO_PORTA,DIO_PIN1,STD_HIGH); //LED2
-	DIO_vidSetPinDirection(DIO_PORTB,DIO_PIN3,STD_HIGH);
-	DIO_vidSetPinDirection(DIO_PORTA,DIO_PIN2,STD_LOW); //Button for toggling LEDs
-	DIO_vidSetPinValue(DIO_PORTA,DIO_PIN2,STD_HIGH);
+	//	DIO_vidSetPinDirection(DIO_PORTB,DIO_PIN0,STD_HIGH); //LED1
+	//	DIO_vidSetPinDirection(DIO_PORTB,DIO_PIN1,STD_HIGH); //LED2
+	DIO_vidSetPinDirection(DIO_PORTB,DIO_PIN3,STD_HIGH); //Intensity-controlled LED
+
+	DIO_vidSetPinDirection(DIO_PORTB,DIO_PIN0,STD_LOW); //Button for toggling LEDs
+	DIO_vidSetPinValue(DIO_PORTB,DIO_PIN0,STD_HIGH);
 
 	//Two buttons for intensity control
-	DIO_vidSetPinDirection(DIO_PORTA,DIO_PIN3,STD_LOW); 
-	DIO_vidSetPinValue(DIO_PORTA,DIO_PIN3,STD_HIGH);
+	DIO_vidSetPinDirection(DIO_PORTB,DIO_PIN1,STD_LOW); 
+	DIO_vidSetPinValue(DIO_PORTB,DIO_PIN1,STD_HIGH);
 
-	DIO_vidSetPinDirection(DIO_PORTA,DIO_PIN4,STD_LOW);
-	DIO_vidSetPinValue(DIO_PORTA,DIO_PIN4,STD_HIGH);
+	DIO_vidSetPinDirection(DIO_PORTB,DIO_PIN2,STD_LOW);
+	DIO_vidSetPinValue(DIO_PORTB,DIO_PIN2,STD_HIGH);
 
 
 	SevenSegment_vidInit(DIO_PORTD,DIO_PORTC);
 	RTO_vidInit();
-
-
+	LCD_vidInit();
 	Task1.periodicity = 500;
 	Task1.first_delay = 0;
 	Task1.state = RTO_U8TASK_RUNNING;
@@ -63,23 +67,28 @@ void main(void) {
 	Task4.state = RTO_U8TASK_PAUSED;
 	Task4.ptrfun = vidCheckButton;
 
-	Task5.periodicity = 200;
+	Task5.periodicity = 50;
 	Task5.first_delay = 0;
 	Task5.state = RTO_U8TASK_RUNNING;
 	Task5.ptrfun = vidDecreaseIntensity;
 
-	Task6.periodicity = 200;
+	Task6.periodicity = 50;
 	Task6.first_delay = 0;
 	Task6.state = RTO_U8TASK_RUNNING;
 	Task6.ptrfun =vidIncreaseIntensity;
 
+	Task7.periodicity = 100;
+	Task7.first_delay = 0;
+	Task7.state = RTO_U8TASK_RUNNING;
+	Task7.ptrfun = vidDisplayIntensity;
 
 	RTO_vidCreateTask(&Task1,1);
 	RTO_vidCreateTask(&Task2,2);
 	RTO_vidCreateTask(&Task3,3);
 	RTO_vidCreateTask(&Task4,4);
 	RTO_vidCreateTask(&Task5,5);
-	RTO_vidCreateTask(&Task6,0);
+	RTO_vidCreateTask(&Task6,6);
+	RTO_vidCreateTask(&Task7,7);
 	while(1);
 
 }
@@ -94,11 +103,11 @@ void vidCount(void) {
 }
 
 void vidToggleLED1(void) {
-	TOGGLE_BIT(PORTA,0);
+//	TOGGLE_BIT(PORTB,0);
 }
 
 void vidCheckButton(void) {
-	if (DIO_u8GetPinValue(DIO_PORTA,DIO_PIN2) == 1) {
+	if (DIO_u8GetPinValue(DIO_PORTB,DIO_PIN0) == 1) {
 		Task3.state = RTO_U8TASK_RUNNING;
 		Task2.state = RTO_U8TASK_PAUSED;
 	}
@@ -109,19 +118,38 @@ void vidCheckButton(void) {
 }
 
 void vidToggleLED2(void) {
-	TOGGLE_BIT(PORTA,1);
+	TOGGLE_BIT(PORTB,1);
 }
 
 void vidDecreaseIntensity(void) {
-	if(DIO_u8GetPinValue(DIO_PORTA,DIO_PIN3) == 1) {
-		u8Intensity--;
-		OCR0 = u8Intensity;
+	if(DIO_u8GetPinValue(DIO_PORTB,DIO_PIN2) == 1) {
+		s16Intensity--;
+		if (s16Intensity <= 0) {
+			s16Intensity = 0;
+		}
+		else {
+			OCR0 = s16Intensity;
+		}
 	}
 }
 
 void vidIncreaseIntensity(void) {
-	if (DIO_u8GetPinValue(DIO_PORTA,DIO_PIN4) == 1) {
-		u8Intensity++;
-		OCR0 = u8Intensity;
+
+	if (DIO_u8GetPinValue(DIO_PORTB,DIO_PIN1) == 1) {
+		s16Intensity++;
+		if (s16Intensity >= 255) {
+			s16Intensity = 255;
+		}
+		else {
+			OCR0 = s16Intensity;
+		}
 	}
+}
+
+void vidDisplayIntensity(void) {
+	u8 u8Value;
+	LCD_vidSendCommand(LCD_CLEAR_SCREEN);
+	LCD_vidGoToXY(0,2);
+	u8Value = s16Intensity;
+	LCD_vidWriteNumber((u16)u8Value);
 }
