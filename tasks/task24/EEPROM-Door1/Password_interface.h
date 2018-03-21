bea4a4c12dbb492a47e6fbd81901c8f6b67de87e
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-03-21 22:50:44 +0200

    Adding macro defining location of flag

--- tasks/task24/EEPROM-Door1/Password_interface.h
+++ tasks/task24/EEPROM-Door1/Password_interface.h
@@ -2,12 +2,14 @@
 #define _PASSWORD_INTERFACE_H
 void Password_vidAskID(void);
 void Password_vidShowID(u8);
-void Password_vidSaveID(u8);
+void Password_vidSave(u8);
 void Password_vidSavePassword(u8,u8);
 void Password_vidAskPassword(void);
 void Password_vidRetreiveID(u8);
 void Password_vidReenterPassword(void);
 u8 Password_vidCheckPasswordMatch(u8);
 void Password_vidReadPassword(u8,u8);
+
+#define FLAG_LOCATION 200
 
 #endif
