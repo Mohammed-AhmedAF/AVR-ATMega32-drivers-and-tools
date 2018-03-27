7a7c55dbe42bd94c89e90564d90fc7a2e433e660
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-03-27 22:00:36 +0200

    Removing unused code, that was displaying the value returned by ADC

--- tests/week13/ADC/main.c
+++ tests/week13/ADC/main.c
@@ -5,9 +5,6 @@
 #include "ADC_interface.h"
 #include <util/delay.h>
 
-u8 s8GetResultSize(void);
-void vidShowResult(void);
-
 u16 res = 0;
 u8 x;
 u32 Calc_s32Power(u8,u8);
@@ -28,49 +25,3 @@ void main(void) {
 
 }
 
-u8 s8GetResultSize(void) {
-	u32 n = res;
-	u8 count = 0;
-	while(n != 0)
-	{
-		n /= 10;
-		++count;
-	}
-	return count;
-}
-
-u32 Calc_s32Power(u8 x, u8 y) {
-	u32 res = 1;	
-	for (u8 i = 1; i <= y; i++) {
-		res = res*x;
-	}
-	return res;
-}
-
-void vidShowResult(void) {
-	u8 u8a;
-	u8 resSize;
-	if (res > 9) {
-		resSize = s8GetResultSize();
-		u32 x = Calc_s32Power(10,resSize-1);	
-		for (u8 i = 1; i <= resSize; i++) {
-			u8a = (res/x)+48;
-			res = res - (res/x)*x;
-			LCD_vidWriteCharacter(u8a);
-			_delay_ms(500);
-			x = x/10;
-		}
-	}
-	else if (res > 100) {
-		LCD_vidWriteCharacter('1');
-	}
-	else if (0 < res) {
-		u8 u8Res = res+48;
-		LCD_vidWriteCharacter(u8Res);
-	}
-	else {
-		LCD_vidWriteCharacter('0');
-		res = res*(-1);
-		vidShowResult();	
-	}
-}
