d88e7669fc0dc8a315c691d89ffe776127339f37
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-04-06 15:49:27 +0200

    SERVICES module: Changin guard

--- lib/SERVICES_interface.h
+++ lib/SERVICES_interface.h
@@ -1,5 +1,5 @@
-#ifndef SERVICES_INTERFACE_H
-#define SERVICES_INTERFACE_H
+#ifndef _SERVICES_INTERFACE_H
+#define _SERVICES_INTERFACE_H
 void Services_vidWriteCharacter(void);
 u8 u8keyPressed;
 #endif
