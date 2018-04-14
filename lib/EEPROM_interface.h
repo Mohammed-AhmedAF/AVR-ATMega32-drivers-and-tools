be32373557cb74caa4a88a62d90228003914f4e7
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-04-14 10:58:33 +0200

    EEPROM driver: Changing guard

--- lib/EEPROM_interface.h
+++ lib/EEPROM_interface.h
@@ -1,5 +1,5 @@
-#ifndef EEPROM_INTERFACE
-#define EEPROM_INTERFACE
+#ifndef _EEPROM_INTERFACE_H
+#define _EEPROM_INTERFACE_H
  void EEPROM_vidInit(void);
  u8 EEPROM_u8WriteByte(u16,u8);
  u8 EEPROM_u8ReadByte(u16,u8 *);
