1d96a3df04f8d4e5e314f975a6c98cac265d2613
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2018-11-11 21:54:07 +0200

    Adding DIO_private.h

---
+++ lib/DIO_private.h
@@ -0,0 +1,19 @@
+#ifndef _DIO_PRIVATE_H
+
+#define DDRA *((u8*) 0x3A)
+#define PORTA *((u8*) 0x3B)
+#define PINA *((u8*) 0x39)
+
+#define DDRB *((u8*) 0x37)
+#define PORTB *((u8*) 0x38)
+#define PINB *((u8*) 0x36)
+
+#define DDRC *((u8*) 0x34) 
+#define PORTC *((u8*) 0x35)
+#define PINC *((u8*) 0x33)
+
+#define DDRD *((u8*) 0x31)
+#define PORTD *((u8*) 0x32)
+#define PIND *((u8*) 0x30)
+
+#define _DIO_PRIVATE_H
