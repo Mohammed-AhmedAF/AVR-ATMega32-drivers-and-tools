2e7be7a5d5d688b726e401da2500f3545acd8ced
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2019-01-27 12:28:36 +0200

    Timer0 driver: Using switch instead of if

--- lib/TIMER0_prog.c
+++ lib/TIMER0_prog.c
@@ -9,80 +9,86 @@
 #include "TIMER0_interface.h"
 
 void TIMER0_vidInit(u8 u8WGMCpy, u8 u8COMCpy, u8 u8ClkCpy) {
-	//Configuration Wave Generation Mode
-	if (u8WGMCpy == TIMER0_WGM_NORMAL) {
-		CLEAR_BIT(TCCR0,6);
-		CLEAR_BIT(TCCR0,3);
-	}
-	if (u8WGMCpy == TIMER0_WGM_CTC) {
-		CLEAR_BIT(TCCR0,6);
-		SET_BIT(TCCR0,3);
-	}
-	if (u8WGMCpy == TIMER0_WGM_PWM) {
-		CLEAR_BIT(TCCR0,6);
-		SET_BIT(TCCR0,3);
-	}
-	if (u8WGMCpy == TIMER0_WGM_FPWM) {
-		SET_BIT(TCCR0,6);
-		SET_BIT(TCCR0,3);
+	//Configuring Wave Generation Mode
+	switch(u8WGMCpy) {
+		case TIMER0_WGM_NORMAL:
+			CLEAR_BIT(TCCR0,6);
+			CLEAR_BIT(TCCR0,3);
+			break;
+		case TIMER0_WGM_CTC:
+			CLEAR_BIT(TCCR0,6);
+			SET_BIT(TCCR0,3);
+			break;
+		case TIMER0_WGM_PWM:
+			SET_BIT(TCCR0,6);
+			CLEAR_BIT(TCCR0,3);
+			break;
+		case TIMER0_WGM_FPWM:
+			SET_BIT(TCCR0,6);
+			SET_BIT(TCCR0,3);
+			break;
 	}
 	//Configuring Compare Output Mode
-	if (u8COMCpy == TIMER0_COM_NORMAL) {
-		CLEAR_BIT(TCCR0,5);
-		CLEAR_BIT(TCCR0,4);		
-	}
-	if (u8COMCpy == TIMER0_COM_TOGGLE) {
-		CLEAR_BIT(TCCR0,5);
-		SET_BIT(TCCR0,4);
-	}
-	if (u8COMCpy == TIMER0_COM_CLEAR) {
-		SET_BIT(TCCR0,5);
-		CLEAR_BIT(TCCR0,4);
-	}
-	if (u8COMCpy == TIMER0_COM_SET) {
-		SET_BIT(TCCR0,5);
-		SET_BIT(TCCR0,4);
+	switch(u8COMCpy) {
+		case TIMER0_COM_NORMAL:
+			CLEAR_BIT(TCCR0,5);
+			CLEAR_BIT(TCCR0,4);		
+			break;
+		case TIMER0_COM_TOGGLE: 
+			CLEAR_BIT(TCCR0,5);
+			SET_BIT(TCCR0,4);
+			break;
+		case TIMER0_COM_CLEAR: 
+			SET_BIT(TCCR0,5);
+			CLEAR_BIT(TCCR0,4);
+			break;
+		case TIMER0_COM_SET:
+			SET_BIT(TCCR0,5);
+			SET_BIT(TCCR0,4);
+			break;
 	}
 	//Configuring Clock
-	if (u8ClkCpy == TIMER0_CLK_0) {
-		CLEAR_BIT(TCCR0,0);
-		CLEAR_BIT(TCCR0,1);
-		CLEAR_BIT(TCCR0,2);
-	}
-	if (u8ClkCpy == TIMER0_CLK_1) {
-		SET_BIT(TCCR0,0);
-		CLEAR_BIT(TCCR0,1);
-		CLEAR_BIT(TCCR0,2);
-	}
-	if (u8ClkCpy == TIMER0_CLK_8) {
-		CLEAR_BIT(TCCR0,0);
-		SET_BIT(TCCR0,1);
-		CLEAR_BIT(TCCR0,2);
-	}
-	if (u8ClkCpy == TIMER0_CLK_64) {
-		SET_BIT(TCCR0,0);
-		SET_BIT(TCCR0,1);
-		CLEAR_BIT(TCCR0,2);
-	}
-	if (u8ClkCpy == TIMER0_CLK_256) {
-		CLEAR_BIT(TCCR0,0);
-		CLEAR_BIT(TCCR0,1);
-		SET_BIT(TCCR0,2);
-	}
-	if (u8ClkCpy == TIMER0_CLK_1024) {
-		SET_BIT(TCCR0,0);
-		CLEAR_BIT(TCCR0,1);
-		SET_BIT(TCCR0,2);
-	}
-	if (u8ClkCpy == TIMER0_CLK_EXT_F) {
-		SET_BIT(TCCR0,0);
-		SET_BIT(TCCR0,1);
-		CLEAR_BIT(TCCR0,2);
-	}
-	if (u8ClkCpy == TIMER0_CLK_EXT_R) {
-		SET_BIT(TCCR0,0);
-		SET_BIT(TCCR0,1);
-		SET_BIT(TCCR0,2);
+	switch(u8ClckCpy) {
+		case TIMER0_CLK_0:
+			CLEAR_BIT(TCCR0,0);
+			CLEAR_BIT(TCCR0,1);
+			CLEAR_BIT(TCCR0,2);
+			break;
+		case TIMER0_CLK_1):
+			SET_BIT(TCCR0,0);
+			CLEAR_BIT(TCCR0,1);
+			CLEAR_BIT(TCCR0,2);
+			break;
+		case TIMER0_CLK_8:
+			CLEAR_BIT(TCCR0,0);
+			SET_BIT(TCCR0,1);
+			CLEAR_BIT(TCCR0,2);
+			break;
+		case TIMER0_CLK_64:
+			SET_BIT(TCCR0,0);
+			SET_BIT(TCCR0,1);
+			CLEAR_BIT(TCCR0,2);
+			break;
+		case TIMER0_CLK_256:
+			CLEAR_BIT(TCCR0,0);
+			CLEAR_BIT(TCCR0,1);
+			SET_BIT(TCCR0,2);
+			break;
+		case TIMER0_CLK_1024:
+			SET_BIT(TCCR0,0);
+			CLEAR_BIT(TCCR0,1);
+			SET_BIT(TCCR0,2);
+			break;
+		case TIMER0_CLK_EXT_F:
+			SET_BIT(TCCR0,0);
+			SET_BIT(TCCR0,1);
+			CLEAR_BIT(TCCR0,2);
+			break;	
+		case TIMER0_CLK_EXT_R:
+			SET_BIT(TCCR0,0);
+			SET_BIT(TCCR0,1);
+			SET_BIT(TCCR0,2);
+			break;
 	}
 }
 
