f9e470427104fb2104f08ad40161edeb71d94a12
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2019-02-15 18:22:46 +0200

    CLOCK: Adding PM/AM functionality

--- lib/CLOCK_interface.h
+++ lib/CLOCK_interface.h
@@ -1,7 +1,22 @@
 #ifndef  _CLOCK_INTERFACE_H
 #define _CLOCK_INTERFACE_H
 
+void CLOCK_vidInit(void);
 void CLOCK_vidCount(void);
 void CLOCK_vidCheckAlarm(void);
+
+/*This macro will be changed according to 
+ *the clock source provided to timer and
+ *the timer MAX count.
+ * */
+#define CLOCK_SEC_OVFCOUNT 31250
+
+/*This macro, if defined, will cause AM/PM to 
+ *be displayed on LCD next to the digits
+ * */
+#define CLOCK_AMPM_SWITCH
+
+#define CLOCK_ALARM_SET 1
+#define CLOCK_ALARM_CLEARED 0
 
 #endif
--- lib/CLOCK_program.c
+++ lib/CLOCK_program.c
@@ -10,24 +10,31 @@
 #include "LCD_interface.h"
 #include "CLOCK_interface.h"
 
-volatile u8 u8AlarmFlag = 1;
+/*Global variables modified in ISR*/
+volatile u8 u8AlarmFlag = CLOCK_ALARM_CLEARED;
 volatile u32 u32OvFCount;
 volatile u8 u8Sec = 0;
 volatile u8 u8Minute = 33;
-volatile u8 u8Hours = 2;
-volatile u8 u8AlarmMinute = 3;
-#define  LCD_XPOS_SHIFT 6
+volatile u8 u8Hours = 21;
+volatile u8 u8AlarmMinute = 35;
+
+
+/*This function will initiate the clock*/
+void CLOCK_vidInit(void) {
+	/*DIO configuration for */	
+	DIO_vidSetPinDirection(DIO_PORTD,DIO_PIN0,DIO_OUTPUT);
+	DIO_vidSetPinValue(DIO_PORTD,DIO_PIN0,STD_LOW);
+}
 
 void CLOCK_vidCount(void) {
 	u32OvFCount++;
-	if (u32OvFCount == 31250) {
+	if (u32OvFCount == CLOCK_SEC_OVFCOUNT) {
 		u32OvFCount = 0;
 		u8Sec++;
 		if (u8Sec == 60) {
 			u8Sec = 0;
 			u8Minute++;
-			if (u8AlarmFlag == 1) {
-				CLOCK_vidCheckAlarm();
+			if (u8AlarmFlag == CLOCK_ALARM_SET) {
 			}
 			if (u8Minute == 60) {
 				u8Minute = 0;
@@ -45,6 +52,16 @@ void CLOCK_vidCount(void) {
 		LCD_vidWriteInPlace(LCD_XPOS_SHIFT+LCD_XPOS5,':');
 		LCD_vidWriteInPlace(LCD_XPOS_SHIFT+LCD_XPOS6,u8Sec/10+'0');
 		LCD_vidWriteInPlace(LCD_XPOS_SHIFT+LCD_XPOS7,u8Sec%10+'0');
+#ifdef CLOCK_AMPM_SWITCH
+		if (u8Hours <= 12) {
+			LCD_vidWriteInPlace(LCD_XPOS_SHIFT+LCD_XPOS9,'A');
+			LCD_vidWriteInPlace(LCD_XPOS_SHIFT+LCD_XPOS10,'M');
+		}
+		else {
+			LCD_vidWriteInPlace(LCD_XPOS_SHIFT+LCD_XPOS9,'p');
+			LCD_vidWriteInPlace(LCD_XPOS_SHIFT+LCD_XPOS10,'M');
+		}
+#endif
 	}
 
 }
@@ -57,8 +74,13 @@ void CLOCK_vidIncrementMinutes(void) {
 	u8Minute++;
 }
 
+/*
+ * This function will be called every minute to check if alarm
+ *value has been reached.
+ * */
 void CLOCK_vidCheckAlarm(void) {
 	if (u8Minute == u8AlarmMinute) {
-		LCD_vidWriteInPlace(LCD_XPOS_SHIFT+LCD_XPOS11,'A');
+		LCD_vidGoToXY(1,2);
+		LCD_vidWriteString("Alarm");
 	}
 }
