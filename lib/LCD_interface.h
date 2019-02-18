ef1082bf50ab4d4ccbeb06dda8d331b890e8e2ba
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2019-02-18 14:10:38 +0200

    LCD:Chaning filenames, changing functionality of vidWriteInPlace()

--- lib/LCD_interface.h
+++ lib/LCD_interface.h
@@ -2,11 +2,11 @@
  * Author: Mohamed Ahmed Abd Al-Fattah
  */
 
-#ifndef LCD_INTERFACE_H
-#define LCD_INTERFACE_H
+#ifndef _LCD_INTERFACE_H
+#define _LCD_INTERFACE_H
 
-#define LCD_DATA_PORT DIO_PORTD
-#define LCD_CTRL_PORT DIO_PORTA
+#define LCD_DATA_PORT DIO_PORTA
+#define LCD_CTRL_PORT DIO_PORTB
 
 #define LCD_D0 0
 #define LCD_D1 1
@@ -33,13 +33,45 @@
 #define LCD_RW 1
 #define LCD_RS 0
 
+/*Macros defined to be used with the GoTOXY function*/
+#define LCD_XPOS0 0
+#define LCD_XPOS1 1
+#define LCD_XPOS2 2
+#define LCD_XPOS3 3
+#define LCD_XPOS4 4
+#define LCD_XPOS5 5
+#define LCD_XPOS6 6
+#define LCD_XPOS7 7
+#define LCD_XPOS8 8
+#define LCD_XPOS9 9
+#define LCD_XPOS10 10
+#define LCD_XPOS11 11
+#define LCD_XPOS12 12
+#define LCD_XPOS13 13
+#define LCD_XPOS14 14
+#define LCD_XPOS15 15
+#define LCD_XPOS16 16
+#define LCD_XPOS17 17
+#define LCD_XPOS18 18
+#define LCD_XPOS19 19
+
+#define LCD_YPOS0 0
+#define LCD_YPOS1 1
+#define LCD_YPOS2 2
+#define LCD_YPOS3 3
+/*This macro is used to shift the clock digits
+ *on LCD row.
+ * */
+#define  LCD_XPOS_SHIFT 5
+
+/*Function definitions*/
 void LCD_vidInit(void);
 void LCD_vidSendCommand(u8);
 void LCD_vidWriteCharacter(u8);
 void LCD_vidWriteString(s8*);
 void LCD_vidBlinkString(s8*,u8);
 void LCD_vidWriteSizedString(s8*,u8);
-void LCD_vidGoToXY(s8,s8);
+void LCD_vidGoToXY(u8,u8);
 void LCD_vidWriteNumber(u16);
-void LCD_vidWriteInPlace(u8,u8);
+void LCD_vidWriteInPlace(u8,u8,u8);
 #endif
