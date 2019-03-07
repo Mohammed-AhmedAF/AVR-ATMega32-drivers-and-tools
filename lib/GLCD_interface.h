6d0533796a079d7711e86534cb21baf4a41f63cb
Mohamed Ahmed Abd Al-Fattah Mahmoud <mohamedstarwars@gmail.com>
2019-03-07 10:07:35 +0200

    GLCD: Turning all command into macros.

--- lib/GLCD_interface.h
+++ lib/GLCD_interface.h
@@ -16,8 +16,8 @@
 #define GLCD_RW_PORT DIO_PORTB
 #define GLCD_EN_PORT DIO_PORTB
 
-#define GLCD_RST_PORT DIO_PORTD
-#define GLCD_CS1_PORT DIO_PORTD
+#define GLCD_RST_PORT DIO_PORTB
+#define GLCD_PSB_PORT DIO_PORTD
 #define GLCD_CS2_PORT DIO_PORTD
 
 
@@ -36,22 +36,22 @@
 #define GLCD_RW_PIN DIO_PIN1
 #define GLCD_EN_PIN DIO_PIN2
 
-#define GLCD_RST_PIN DIO_PIN0
-#define GLCD_CS1_PIN DIO_PIN1
+#define GLCD_RST_PIN DIO_PIN7
+#define GLCD_PSB_PIN DIO_PIN1
 #define GLCD_CS2_PIN DIO_PIN2
 
 /*Definition of GLCD commands*/
-#define GLCD_DISPLAYOFF 0x3E
-#define GLCD_SETYADD0 0x40
-#define GLCD_SETXADD0 0xB8
-#define GLCD_SETZADD0 0xC0
-#define GLCD_DISPLAYON 0x3F
-
+#define GLCD_CLEARSCREEN 0x01
+#define GLCD_RETURN_HOME 0x02
+#define GLCD_DISPLAYON 0x0C
+#define GLCD_DISPLAY_OFF 0x0B
+#define GLCD_FUNCTIONSET 0x30
+#define GLCD_ENTRYMODE 0x06
 
 /*Function definitions*/
 void GLCD_vidInit(void);
 void GLCD_vidSendCommand(u8 u8Command);
 void GLCD_vidWrite(u8 u8Data);
-
-
+void GLCD_vidEnableGraphics(void);
+void GLCD_vidDisbaleGraphics(void);
 #endif
--- lib/GLCD_program.c
+++ lib/GLCD_program.c
@@ -33,7 +33,7 @@ void GLCD_vidInit(void) {
 	DIO_vidSetPinDirection(GLCD_D7_PORT,GLCD_D7_PIN,DIO_OUTPUT);
 
 	/*Setting pin direction for command pits*/
-	DIO_vidSetPinDirection(GLCD_CS1_PORT,GLCD_CS1_PIN,DIO_OUTPUT);
+	DIO_vidSetPinDirection(GLCD_PSB_PORT,GLCD_PSB_PIN,DIO_OUTPUT);
 	DIO_vidSetPinDirection(GLCD_CS2_PORT,GLCD_CS2_PIN,DIO_OUTPUT);
 	DIO_vidSetPinDirection(GLCD_RST_PORT,GLCD_RST_PIN,DIO_OUTPUT);
 
@@ -42,21 +42,31 @@ void GLCD_vidInit(void) {
 	DIO_vidSetPinDirection(GLCD_RW_PORT,GLCD_RW_PIN,DIO_OUTPUT);
 	DIO_vidSetPinDirection(GLCD_EN_PORT,GLCD_EN_PIN,DIO_OUTPUT);
 
-	/*Setting both left and right display*/
-	DIO_vidSetPinValue(GLCD_CS1_PORT,GLCD_CS1_PIN,STD_HIGH);
-	DIO_vidSetPinValue(GLCD_CS2_PORT,GLCD_CS2_PIN,STD_HIGH);
-	DIO_vidSetPinValue(GLCD_RST_PORT,GLCD_RST_PIN,STD_HIGH);
 
 	_delay_ms(20);
 
-	GLCD_vidSendCommand(GLCD_DISPLAYOFF);
-	GLCD_vidSendCommand(GLCD_SETYADD0);	
-	GLCD_vidSendCommand(GLCD_SETXADD0);
-	GLCD_vidSendCommand(GLCD_SETZADD0);
-	GLCD_vidSendCommand(GLCD_DISPLAYON);
+	DIO_vidSetPinValue(GLCD_RST_PORT,GLCD_RST_PIN,STD_LOW);
+	_delay_ms(120);
+	DIO_vidSetPinValue(GLCD_RST_PORT,GLCD_RST_PIN,STD_HIGH);
+	_delay_ms(50);
+
+	_delay_ms(100);
+	GLCD_vidSendCommand(GLCD_FUNCTIONSET); /*Function set*/
+	_delay_ms(120);
+	GLCD_vidSendCommand(GLCD_DISPLAYON);  /*Display on/off control*/
+	_delay_ms(50);
+	GLCD_vidSendCommand(GLCD_FUNCTIONSET); /*Function set*/
+	_delay_ms(120);
+	GLCD_vidSendCommand(GLCD_CLEARSCREEN); /*Clear*/
+	_delay_ms(20);
+	GLCD_vidSendCommand(GLCD_ENTRYMODE); /*Entry mode*/
+	_delay_ms(120);
+
 }
 
 void GLCD_vidSendCommand(u8 u8CommandCpy) {
+	DIO_vidSetPinValue(GLCD_RS_PORT,GLCD_RS_PIN,STD_LOW);
+	DIO_vidSetPinValue(GLCD_RW_PORT,GLCD_RW_PIN,STD_LOW);
 
 	DIO_vidSetPinValue(GLCD_D0_PORT,GLCD_D0_PIN,GET_BIT(u8CommandCpy,0));
 	DIO_vidSetPinValue(GLCD_D1_PORT,GLCD_D2_PIN,GET_BIT(u8CommandCpy,1));
@@ -67,16 +77,18 @@ void GLCD_vidSendCommand(u8 u8CommandCpy) {
 	DIO_vidSetPinValue(GLCD_D6_PORT,GLCD_D6_PIN,GET_BIT(u8CommandCpy,6));
 	DIO_vidSetPinValue(GLCD_D7_PORT,GLCD_D7_PIN,GET_BIT(u8CommandCpy,7));
 
-	DIO_vidSetPinValue(GLCD_RS_PORT,GLCD_RS_PIN,STD_LOW);
-	DIO_vidSetPinValue(GLCD_RW_PORT,GLCD_RW_PIN,STD_LOW);
 
 	DIO_vidSetPinValue(GLCD_EN_PORT,GLCD_EN_PIN,STD_HIGH);
-	_delay_ms(5);
+	_delay_ms(120);
 	DIO_vidSetPinValue(GLCD_EN_PORT,GLCD_EN_PIN,STD_LOW);
-	_delay_ms(5);
+	_delay_ms(120);
 }
 
 void GLCD_vidWrite(u8 u8Data) {
+	DIO_vidSetPinValue(GLCD_RS_PORT,GLCD_RS_PIN,STD_HIGH);
+	DIO_vidSetPinValue(GLCD_RW_PORT,GLCD_RW_PIN,STD_LOW);
+	
+	/*Putting data pits into corresponding pins*/
 	DIO_vidSetPinValue(GLCD_D0_PORT,GLCD_D0_PIN,GET_BIT(u8Data,0));
 	DIO_vidSetPinValue(GLCD_D1_PORT,GLCD_D2_PIN,GET_BIT(u8Data,1));
 	DIO_vidSetPinValue(GLCD_D2_PORT,GLCD_D3_PIN,GET_BIT(u8Data,2));
@@ -86,13 +98,27 @@ void GLCD_vidWrite(u8 u8Data) {
 	DIO_vidSetPinValue(GLCD_D6_PORT,GLCD_D6_PIN,GET_BIT(u8Data,6));
 	DIO_vidSetPinValue(GLCD_D7_PORT,GLCD_D7_PIN,GET_BIT(u8Data,7));
 
-	DIO_vidSetPinValue(GLCD_RS_PORT,GLCD_RS_PIN,STD_HIGH);
-	DIO_vidSetPinValue(GLCD_RW_PORT,GLCD_RW_PIN,STD_LOW);
-
+	
 	DIO_vidSetPinValue(GLCD_EN_PORT,GLCD_EN_PIN,STD_HIGH);
-	_delay_ms(5);
+	_delay_ms(120);
 	DIO_vidSetPinValue(GLCD_EN_PORT,GLCD_EN_PIN,STD_LOW);
-	_delay_ms(5);
+	_delay_ms(120);
 
 }
 
+void GLCD_vidEnableGraphics(void) {
+
+	GLCD_vidSendCommand(0x20);  
+	_delay_ms(1);
+	GLCD_vidSendCommand(0x24); 
+	_delay_ms(1);
+	GLCD_vidSendCommand(0x26);
+	_delay_ms(1);
+	_delay_ms(5);
+}
+
+
+void GLCD_vidDisableGraphics(void) {
+	GLCD_vidSendCommand(0x20);
+	_delay_ms(5);
+}
