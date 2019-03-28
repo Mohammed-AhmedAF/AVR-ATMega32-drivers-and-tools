002135d012d3e3548fb0bfbb54aac2eb84c26d68
Mohammed Ahmed <mohamedstarwars@gmail.com>
2019-03-28 14:16:47 +0200

    UART: Correcting private file

--- lib/UART_private.h
+++ lib/UART_private.h
@@ -1,9 +1,9 @@
 #ifndef _UART_PRIVATE_H
 #define _UART_PRIVATE_H
 
-#define UCSRB * ((u32*) 0x2A)
-#define UCSRA * ((u32*) 0x2B)
-#define UBRRL * ((u32*) 0x29)
-#define UDR * ((u32*)) 0x2C)
+#define UCSRB * ((u8*) 0x2A)
+#define UCSRA * ((u8*) 0x2B)
+#define UBRRL * ((u8*) 0x29)
+#define UDR * ((u8*)) 0x2C)
 
 #endif
