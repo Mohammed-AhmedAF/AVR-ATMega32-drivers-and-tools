f3f208c3cf9e8cbb93582ff902ae476b3fb73c9d
Mohammed Ahmed <mohamedstarwars@gmail.com>
2019-05-20 11:10:37 +0200

    Working version of GLCD driver, based on code from electronicswings.com

--- lib/GLCD_interface.h
+++ lib/GLCD_interface.h
@@ -1,8 +1,32 @@
-#ifndef _GLCD_INTERFACE_H
-#define _GLCD_INTERFACE_H
+/*
+ * GLCD_interface.h
+ *
+ * Created: 5/20/2019 9:16:13 AM
+ *  Author: Admin
+ */ 
 
-/*GLCD port number definition*/
 
+#ifndef _GLCD_INTERFACE_H_
+#define _GLCD_INTERFACE_H_
+
+
+/*Command port definitions*/
+#define GLCD_EN_PORT DIO_PORTB
+#define GLCD_RW_PORT DIO_PORTB
+#define GLCD_RS_PORT DIO_PORTB
+#define GLCD_CS1_PORT DIO_PORTB
+#define GLCD_CS2_PORT DIO_PORTB
+#define GLCD_RST_PORT DIO_PORTB
+
+/*Command pin definitions*/
+#define GLCD_EN_PIN DIO_PIN2
+#define GLCD_RW_PIN DIO_PIN1
+#define GLCD_RS_PIN DIO_PIN0
+#define GLCD_CS1_PIN DIO_PIN4
+#define GLCD_CS2_PIN DIO_PIN5
+#define GLCD_RST_PIN DIO_PIN3
+
+/*Data port definitions*/
 #define GLCD_D0_PORT DIO_PORTA
 #define GLCD_D1_PORT DIO_PORTA
 #define GLCD_D2_PORT DIO_PORTA
@@ -12,16 +36,7 @@
 #define GLCD_D6_PORT DIO_PORTA
 #define GLCD_D7_PORT DIO_PORTA
 
-#define GLCD_RS_PORT DIO_PORTB
-#define GLCD_RW_PORT DIO_PORTB
-#define GLCD_EN_PORT DIO_PORTB
-
-#define GLCD_RST_PORT DIO_PORTB
-#define GLCD_PSB_PORT DIO_PORTD
-#define GLCD_CS2_PORT DIO_PORTD
-
-
-/*GLCD pin number definition*/
+/*Data pin definitions*/
 #define GLCD_D0_PIN DIO_PIN0
 #define GLCD_D1_PIN DIO_PIN1
 #define GLCD_D2_PIN DIO_PIN2
@@ -31,27 +46,13 @@
 #define GLCD_D6_PIN DIO_PIN6
 #define GLCD_D7_PIN DIO_PIN7
 
+/*Other definitions*/
+#define GLCD_TOTALPAGES 8
 
-#define GLCD_RS_PIN DIO_PIN0
-#define GLCD_RW_PIN DIO_PIN1
-#define GLCD_EN_PIN DIO_PIN2
-
-#define GLCD_RST_PIN DIO_PIN7
-#define GLCD_PSB_PIN DIO_PIN1
-#define GLCD_CS2_PIN DIO_PIN2
-
-/*Definition of GLCD commands*/
-#define GLCD_CLEARSCREEN 0x01
-#define GLCD_RETURN_HOME 0x02
-#define GLCD_DISPLAYON 0x0C
-#define GLCD_DISPLAY_OFF 0x0B
-#define GLCD_FUNCTIONSET 0x30
-#define GLCD_ENTRYMODE 0x06
-
-/*Function definitions*/
 void GLCD_vidInit(void);
-void GLCD_vidSendCommand(u8 u8Command);
-void GLCD_vidWrite(u8 u8Data);
-void GLCD_vidEnableGraphics(void);
-void GLCD_vidDisbaleGraphics(void);
-#endif
+void GLCD_vidSendCommand(u8 u8CommandCpy);
+void GLCD_vidWriteCharacter(u8 u8CharacterCpy);
+void GLCD_vidWriteString(u8, u8 *);
+void GLCD_vidClearAll(void);
+
+#endif /* GLCD_INTERFACE_H_ */
--- lib/GLCD_program.c
+++ lib/GLCD_program.c
@@ -1,28 +1,20 @@
 /*
- * Author: Mohamed Ahmed Abd Al Fattah
- * Purpose: Graphical LCD driver
+ * GLCD_program.c
  *
- */
-
-/* 
- * CS1, CS2 are chip selects, those are used
- * to select one of two chips controlling the
- * two segments of the display. Different GLCD modules differ in
- * the application of these two pins.
- *
- *
- * RS: Register Select, Data/Instruction select.
- */
+ * Created: 5/20/2019 9:15:11 AM
+ *  Author: Admin
+ */ 
 
 #include "Macros.h"
 #include "Std_Types.h"
 #include "DIO_interface.h"
+#include "Font_Header.h"
+#include <util/delay.h>
 #include "GLCD_interface.h"
-#include "util/delay.h"
 
 
 void GLCD_vidInit(void) {
-	/*Setting pin direction*/
+	/*Configuring direction for data pins*/
 	DIO_vidSetPinDirection(GLCD_D0_PORT,GLCD_D0_PIN,DIO_OUTPUT);
 	DIO_vidSetPinDirection(GLCD_D1_PORT,GLCD_D1_PIN,DIO_OUTPUT);
 	DIO_vidSetPinDirection(GLCD_D2_PORT,GLCD_D2_PIN,DIO_OUTPUT);
@@ -31,94 +23,155 @@ void GLCD_vidInit(void) {
 	DIO_vidSetPinDirection(GLCD_D5_PORT,GLCD_D5_PIN,DIO_OUTPUT);
 	DIO_vidSetPinDirection(GLCD_D6_PORT,GLCD_D6_PIN,DIO_OUTPUT);
 	DIO_vidSetPinDirection(GLCD_D7_PORT,GLCD_D7_PIN,DIO_OUTPUT);
-
-	/*Setting pin direction for command pits*/
-	DIO_vidSetPinDirection(GLCD_PSB_PORT,GLCD_PSB_PIN,DIO_OUTPUT);
-	DIO_vidSetPinDirection(GLCD_CS2_PORT,GLCD_CS2_PIN,DIO_OUTPUT);
-	DIO_vidSetPinDirection(GLCD_RST_PORT,GLCD_RST_PIN,DIO_OUTPUT);
-
-	/*Setting direction for pins RS,RW,EN*/
+	
+	/*Configuring direction for control pins*/
 	DIO_vidSetPinDirection(GLCD_RS_PORT,GLCD_RS_PIN,DIO_OUTPUT);
 	DIO_vidSetPinDirection(GLCD_RW_PORT,GLCD_RW_PIN,DIO_OUTPUT);
 	DIO_vidSetPinDirection(GLCD_EN_PORT,GLCD_EN_PIN,DIO_OUTPUT);
-
-
-	_delay_ms(20);
-
-	DIO_vidSetPinValue(GLCD_RST_PORT,GLCD_RST_PIN,STD_LOW);
-	_delay_ms(120);
+	
+	/*Giving initialization commands*/
+	DIO_vidSetPinValue(GLCD_CS1_PORT,GLCD_CS1_PIN,STD_HIGH);
+	DIO_vidSetPinValue(GLCD_CS2_PORT,GLCD_CS2_PIN,STD_HIGH);
 	DIO_vidSetPinValue(GLCD_RST_PORT,GLCD_RST_PIN,STD_HIGH);
-	_delay_ms(50);
-
-	_delay_ms(100);
-	GLCD_vidSendCommand(GLCD_FUNCTIONSET); /*Function set*/
-	_delay_ms(120);
-	GLCD_vidSendCommand(GLCD_DISPLAYON);  /*Display on/off control*/
-	_delay_ms(50);
-	GLCD_vidSendCommand(GLCD_FUNCTIONSET); /*Function set*/
-	_delay_ms(120);
-	GLCD_vidSendCommand(GLCD_CLEARSCREEN); /*Clear*/
-	_delay_ms(20);
-	GLCD_vidSendCommand(GLCD_ENTRYMODE); /*Entry mode*/
-	_delay_ms(120);
-
+	
+	_delay_us(20);
+	
+	GLCD_vidSendCommand(0x3E);
+	GLCD_vidSendCommand(0x40);
+	GLCD_vidSendCommand(0xB8);
+	GLCD_vidSendCommand(0xC0);
+	GLCD_vidSendCommand(0x3F);
 }
 
 void GLCD_vidSendCommand(u8 u8CommandCpy) {
-	DIO_vidSetPinValue(GLCD_RS_PORT,GLCD_RS_PIN,STD_LOW);
+	DIO_vidSetPinValue(GLCD_EN_PORT,GLCD_EN_PORT,STD_HIGH);
 	DIO_vidSetPinValue(GLCD_RW_PORT,GLCD_RW_PIN,STD_LOW);
-
+	DIO_vidSetPinValue(GLCD_RS_PORT,GLCD_RS_PIN,STD_LOW);
+	
+	/*Filling in the command to be sent*/
 	DIO_vidSetPinValue(GLCD_D0_PORT,GLCD_D0_PIN,GET_BIT(u8CommandCpy,0));
-	DIO_vidSetPinValue(GLCD_D1_PORT,GLCD_D2_PIN,GET_BIT(u8CommandCpy,1));
-	DIO_vidSetPinValue(GLCD_D2_PORT,GLCD_D3_PIN,GET_BIT(u8CommandCpy,2));
+	DIO_vidSetPinValue(GLCD_D1_PORT,GLCD_D1_PIN,GET_BIT(u8CommandCpy,1));
+	DIO_vidSetPinValue(GLCD_D2_PORT,GLCD_D2_PIN,GET_BIT(u8CommandCpy,2));
 	DIO_vidSetPinValue(GLCD_D3_PORT,GLCD_D3_PIN,GET_BIT(u8CommandCpy,3));
 	DIO_vidSetPinValue(GLCD_D4_PORT,GLCD_D4_PIN,GET_BIT(u8CommandCpy,4));
 	DIO_vidSetPinValue(GLCD_D5_PORT,GLCD_D5_PIN,GET_BIT(u8CommandCpy,5));
 	DIO_vidSetPinValue(GLCD_D6_PORT,GLCD_D6_PIN,GET_BIT(u8CommandCpy,6));
 	DIO_vidSetPinValue(GLCD_D7_PORT,GLCD_D7_PIN,GET_BIT(u8CommandCpy,7));
-
-
-	DIO_vidSetPinValue(GLCD_EN_PORT,GLCD_EN_PIN,STD_HIGH);
-	_delay_ms(120);
+	
+	_delay_ms(5);
 	DIO_vidSetPinValue(GLCD_EN_PORT,GLCD_EN_PIN,STD_LOW);
-	_delay_ms(120);
+	_delay_us(5);	
 }
 
-void GLCD_vidWrite(u8 u8Data) {
+void GLCD_vidWriteCharacter(u8 u8CharacterCpy) {
 	DIO_vidSetPinValue(GLCD_RS_PORT,GLCD_RS_PIN,STD_HIGH);
 	DIO_vidSetPinValue(GLCD_RW_PORT,GLCD_RW_PIN,STD_LOW);
-	
-	/*Putting data pits into corresponding pins*/
-	DIO_vidSetPinValue(GLCD_D0_PORT,GLCD_D0_PIN,GET_BIT(u8Data,0));
-	DIO_vidSetPinValue(GLCD_D1_PORT,GLCD_D2_PIN,GET_BIT(u8Data,1));
-	DIO_vidSetPinValue(GLCD_D2_PORT,GLCD_D3_PIN,GET_BIT(u8Data,2));
-	DIO_vidSetPinValue(GLCD_D3_PORT,GLCD_D3_PIN,GET_BIT(u8Data,3));
-	DIO_vidSetPinValue(GLCD_D4_PORT,GLCD_D4_PIN,GET_BIT(u8Data,4));
-	DIO_vidSetPinValue(GLCD_D5_PORT,GLCD_D5_PIN,GET_BIT(u8Data,5));
-	DIO_vidSetPinValue(GLCD_D6_PORT,GLCD_D6_PIN,GET_BIT(u8Data,6));
-	DIO_vidSetPinValue(GLCD_D7_PORT,GLCD_D7_PIN,GET_BIT(u8Data,7));
-
-	
 	DIO_vidSetPinValue(GLCD_EN_PORT,GLCD_EN_PIN,STD_HIGH);
-	_delay_ms(120);
+	
+	/*Filling in the character to be written*/
+	DIO_vidSetPinValue(GLCD_D0_PORT,GLCD_D0_PIN,GET_BIT(u8CharacterCpy,0));
+	DIO_vidSetPinValue(GLCD_D1_PORT,GLCD_D1_PIN,GET_BIT(u8CharacterCpy,1));
+	DIO_vidSetPinValue(GLCD_D2_PORT,GLCD_D2_PIN,GET_BIT(u8CharacterCpy,2));
+	DIO_vidSetPinValue(GLCD_D3_PORT,GLCD_D3_PIN,GET_BIT(u8CharacterCpy,3));
+	DIO_vidSetPinValue(GLCD_D4_PORT,GLCD_D4_PIN,GET_BIT(u8CharacterCpy,4));
+	DIO_vidSetPinValue(GLCD_D5_PORT,GLCD_D5_PIN,GET_BIT(u8CharacterCpy,5));
+	DIO_vidSetPinValue(GLCD_D6_PORT,GLCD_D6_PIN,GET_BIT(u8CharacterCpy,6));
+	DIO_vidSetPinValue(GLCD_D7_PORT,GLCD_D7_PIN,GET_BIT(u8CharacterCpy,7));
+	
+	_delay_us(5);
 	DIO_vidSetPinValue(GLCD_EN_PORT,GLCD_EN_PIN,STD_LOW);
-	_delay_ms(120);
-
+	_delay_us(5);
 }
 
-void GLCD_vidEnableGraphics(void) {
-
-	GLCD_vidSendCommand(0x20);  
-	_delay_ms(1);
-	GLCD_vidSendCommand(0x24); 
-	_delay_ms(1);
-	GLCD_vidSendCommand(0x26);
-	_delay_ms(1);
-	_delay_ms(5);
-}
-
-
-void GLCD_vidDisableGraphics(void) {
-	GLCD_vidSendCommand(0x20);
-	_delay_ms(5);
-}
+void GLCD_vidClearAll(void) {
+		u8 i, j;
+	/*Selecting both halves of the display*/
+	DIO_vidSetPinValue(GLCD_CS1_PORT,GLCD_CS1_PIN,STD_HIGH);
+	DIO_vidSetPinValue(GLCD_CS2_PORT,GLCD_CS1_PIN,STD_HIGH);
+	
+	for (i = 0; i < GLCD_TOTALPAGES; i++) {
+		GLCD_vidSendCommand((0xB8)+i);
+		for (j = 0; j < 64; j++) {
+			GLCD_vidWriteCharacter(0);
+		}
+	}
+		GLCD_vidSendCommand(0x40); /*Setting x address to 0*/
+		GLCD_vidSendCommand(0xB8);  /*Setting y address to 0*/
+}	
+	
+void GLCD_vidWriteString(u8 page_no, u8 *str)			/* GLCD string write function */
+{
+	unsigned int i, column;
+	unsigned int  Page = ((0xB8) + page_no);
+	unsigned int Y_address = 0;
+	float Page_inc = 0.5;
+	
+	DIO_vidSetPinValue(GLCD_CS1_PORT,GLCD_CS1_PIN,STD_HIGH);
+	DIO_vidSetPinValue(GLCD_CS2_PORT,GLCD_CS2_PIN,STD_LOW);
+	
+	GLCD_vidSendCommand(Page);
+	for(i = 0; str[i] != 0; i++)					/* Print each char in string till null */
+	{
+		if (Y_address > (1024-(((page_no)*128)+FontWidth))) /* Check Whether Total Display get overflowed */
+		break;										/* If yes then break writing */
+		if (str[i]!=32)								/* Check whether character is not a SPACE */
+		{
+			for (column=1; column<=FontWidth; column++)
+			{
+				if ((Y_address+column)==(128*((int)(Page_inc+0.5))))	/* If yes then check whether it overflow from right side of display */
+				{
+					if (column == FontWidth)		/* Also check and break if it overflow after 5th column */
+					break;
+					GLCD_vidSendCommand(0x40);				/* If not 5th and get overflowed then change Y address to START column */
+					Y_address = Y_address + column;	/* Increment Y address count by column no. */
+					TOGGLE_BIT(PORTB,GLCD_CS1_PIN); /* If yes then change segment controller to display on other half of display */
+					TOGGLE_BIT(PORTB,GLCD_CS2_PIN);
+					GLCD_vidSendCommand(Page + Page_inc);	/* Execute command for page change */
+					Page_inc = Page_inc + 0.5;		/* Increment Page No. by half */
+				}
+			}
+		}
+		if (Y_address>(1024-(((page_no)*128)+FontWidth)))   /* Check Whether Total Display get overflowed */
+		break;										/* If yes then break writing */
+		if((font[((str[i]-32)*FontWidth)+4])==0 || str[i]==32)/* Check whether character is SPACE or character last column is zero */
+		{
+			for(column=0; column<FontWidth; column++)
+			{
+				GLCD_vidWriteCharacter(font[str[i]-32][column]);	/* If yes then then print character */
+				if((Y_address+1)%64==0)				/* check whether it gets overflowed  from either half of side */
+				{
+					TOGGLE_BIT(PORTB,GLCD_CS1_PIN); /* If yes then change segment controller to display on other half of display */
+					TOGGLE_BIT(PORTB,GLCD_CS2_PIN);
+					GLCD_vidSendCommand((Page+Page_inc));	/* Execute command for page change */
+					Page_inc = Page_inc + 0.5;		/* Increment Page No. by half */
+				}
+				Y_address++;						/* Increment Y_address count per column */
+			}
+		}
+		else										/* If character is not SPACE or character last column is not zero */
+		{
+			for(column=0; column<FontWidth; column++)
+			{
+				GLCD_vidWriteCharacter(font[str[i]-32][column]); /* Then continue to print hat char */
+				if((Y_address+1)%64==0)				/* check whether it gets overflowed  from either half of side */
+				{
+					TOGGLE_BIT(PORTB,GLCD_CS1_PIN); /* If yes then change segment controller to display on other half of display */
+					TOGGLE_BIT(PORTB,GLCD_CS2_PIN);
+					GLCD_vidSendCommand((Page+Page_inc));	/* Execute command for page change */
+					Page_inc = Page_inc + 0.5;		/* Increment Page No. by half */
+				}
+				Y_address++;						/* Increment Y_address count per column */
+			}
+			GLCD_vidWriteCharacter(0);							/* Add one column of zero to print next character next of zero */
+			Y_address++;							/* Increment Y_address count for last added zero */
+			if((Y_address)%64 == 0)					/* check whether it gets overflowed  from either half of side */
+			{
+				TOGGLE_BIT(PORTB,GLCD_CS1_PIN); /* If yes then change segment controller to display on other half of display */
+				TOGGLE_BIT(PORTB,GLCD_CS2_PIN);
+				GLCD_vidSendCommand((Page+Page_inc));		/* Execute command for page change */
+				Page_inc = Page_inc + 0.5;			/* Increment Page No. by half */
+			}
+		}
+	}
+	GLCD_vidSendCommand(0x40);								/* Set Y address (column=0) */
+}		
