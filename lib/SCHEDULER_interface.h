f4f7e67707b2b62552a0a9cee64783cdd5eda106
Mohammed Ahmed <mohamedstarwars@gmail.com>
2019-06-26 11:17:51 +0200

    SCHEDULER: New driver, same as RTO but with better naming

---
+++ lib/SCHEDULER_interface.h
@@ -0,0 +1,47 @@
+/*
+ * SCHEDULER_interface.h
+ *
+ * Created: 6/25/2019 3:00:31 PM
+ *  Author: Admin
+ */ 
+
+
+#ifndef _SCHEDULER_INTERFACE_H_
+#define _SCHEDULER_INTERFACE_H_
+
+#define SCHEDULER_ONETICK 31
+#define SCHEDULER_MAXTASKS 20
+#define SCHEDULER_NULLTASK 0
+#define SCHEDULER_TASKSTATE_RUNNING 1
+#define SCHEDULER_TASKSTATE_STOPPED 0
+
+/*Task definitions*/
+#define SCHEDULER_TASK0 0
+#define SCHEDULER_TASK1 1
+#define SCHEDULER_TASK2 2
+#define SCHEDULER_TASK3 3
+#define SCHEDULER_TASK4 4
+#define SCHEDULER_TASK5 5
+#define SCHEDULER_TASK6 6
+#define SCHEDULER_TASK7 7
+#define SCHEDULER_TASK8 8
+#define SCHEDULER_TASK9 9
+#define SCHEDULER_TASK10 10
+#define SCHEDULER_TASK11 11
+
+typedef struct {
+	u16 u16FirstDelay;
+	u16  u16Periodicity; //If u16Periodicity = 0, then it will not repeat
+	void (*ptrfun)(void);
+	u8 u8State;
+} Task_Type; 
+
+
+
+void SCHEDULER_vidInit(void);
+void SCHEDULER_vidCountOneTick(void);
+void SCHEDULER_vidSchedule(void);
+void SCHEDULER_vidCreateTask(Task_Type *,u8);
+
+
+#endif /* SCHEDULER_INTERFACE_H_ */
---
+++ lib/SCHEDULER_program.c
@@ -0,0 +1,56 @@
+/*
+ * SCHEDULER_program.c
+ *
+ * Created: 6/25/2019 2:49:18 PM
+ *  Author: Admin
+ */ 
+
+#include "Std_Types.h"
+#include "Macros.h"
+#include "DIO_interface.h"
+#include "TIMER0_interface.h"
+#include "INTERRUPTS_interface.h"
+#include "SCHEDULER_interface.h"
+
+
+Task_Type * Tasks_Array[SCHEDULER_MAXTASKS];
+
+volatile u8 u8OVFCount;
+
+void SCHEDULER_vidInit(void) {
+	TIMER0_vidInit(TIMER0_WGM_NORMAL,TIMER0_COM_NORMAL,TIMER0_CLK_1);
+	
+	INTERRUPTS_vidEnableInterrupt(INTERRUPTS_TIMER0_OVF);
+	INTERRUPTS_vidPutISRFunction(TIMER0_OVF_VECT,SCHEDULER_vidCountOneTick);
+	
+}
+
+void SCHEDULER_vidCreateTask(Task_Type * task, u8 u8TaskPriority) {
+	Tasks_Array[u8TaskPriority] = task;
+}
+
+void SCHEDULER_vidSchedule(void) {
+	u8 u8TaskIndex = 0;
+	for (u8TaskIndex = 0; u8TaskIndex < SCHEDULER_MAXTASKS; u8TaskIndex++) {
+		if (Tasks_Array[u8TaskIndex] != SCHEDULER_NULLTASK) {
+			if (Tasks_Array[u8TaskIndex]->u8State == SCHEDULER_TASKSTATE_RUNNING) {
+				if (Tasks_Array[u8TaskIndex]->u16FirstDelay == 0) {
+					Tasks_Array[u8TaskIndex]->ptrfun();
+					Tasks_Array[u8TaskIndex]->u16FirstDelay = Tasks_Array[u8TaskIndex]->u16Periodicity-1;					
+				}
+				else {
+					Tasks_Array[u8TaskIndex]->u16FirstDelay--;
+				}
+			}
+		}
+	}
+}
+
+void SCHEDULER_vidCountOneTick(void) {
+	u8OVFCount++;
+	if (u8OVFCount == SCHEDULER_ONETICK) {
+		u8OVFCount = 0;
+		SCHEDULER_vidSchedule();
+	}
+}
+
