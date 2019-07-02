b214d02c30f1fdf01c7512837f7eb40f9887efa8
Mohammed Ahmed <mohamedstarwars@gmail.com>
2019-07-02 11:40:20 +0200

    DIO: Adding volatile keyword to register definitions

--- lib/DIO_private.h
+++ lib/DIO_private.h
@@ -3,31 +3,31 @@
 
 /*Register definitions*/
 #define DDRA *((u8*) 0x3A)
-#define PORTA *((u8*) 0x3B)
+#define PORTA *((volatile u8*) 0x3B)
 #define PINA *((u8*) 0x39)
 
 #define DDRB *((u8*) 0x37)
-#define PORTB *((u8*) 0x38)
+#define PORTB *((volatile u8*) 0x38)
 #define PINB *((u8*) 0x36)
 
 #define DDRC *((u8*) 0x34) 
-#define PORTC *((u8*) 0x35)
+#define PORTC *((volatile u8*) 0x35)
 #define PINC *((u8*) 0x33)
 
 #define DDRD *((u8*) 0x31)
-#define PORTD *((u8*) 0x32)
+#define PORTD *((volatile u8*) 0x32)
 #define PIND *((u8*) 0x30)
 
 #define DDRE *((u8*) 0x22)
-#define PORTE *((u8*) 0x23)
+#define PORTE *((volatile u8*) 0x23)
 #define PINE *((u8*) 0x21)
 
 #define DDRF *((u8*) 0x61)
-#define PORTF *((u8*) 0x62)
+#define PORTF *((volatile u8*) 0x62)
 #define PINF *((u8*) 0x20)
 
 #define DDRG *((u8*) 0x64)
-#define PORTG *((u8*) 0x65)
+#define PORTG *((volatile u8*) 0x65)
 #define PING *((u8*) 0x63)
 
 #endif
