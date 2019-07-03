c57174327a56acda106db602651e3da694da35a6
Mohammed Ahmed <mohamedstarwars@gmail.com>
2019-07-03 10:42:10 +0200

    TIMER2: Adding volatile to register definitions

--- lib/TIMER2_private.h
+++ lib/TIMER2_private.h
@@ -2,9 +2,9 @@
 #define _TIMER2_PRIVATE_H
 
 
-#define TCCR2 * ((u8*) 0x45)
-#define TCNT2 * ((u8*) 0x44)
-#define OCR2 * ((u8*) 0x43)
+#define TCCR2 * ((volatile u8*) 0x45)
+#define TCNT2 * ((volatile u8*) 0x44)
+#define OCR2 * ((volatile u8*) 0x43)
 
 #endif
 
