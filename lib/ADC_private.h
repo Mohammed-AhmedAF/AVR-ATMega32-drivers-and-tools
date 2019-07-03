e576e2f2878c585135f519035b22f693ef94bec8
Mohammed Ahmed <mohamedstarwars@gmail.com>
2019-07-03 11:02:22 +0200

    ADC: Adding volatile to register definitions

--- lib/ADC_private.h
+++ lib/ADC_private.h
@@ -1,10 +1,9 @@
 #ifndef _ADC_PRIVATE_H
 #define _ADC_PRIVATE_H
 
-#define ADMUX *((u8*) 0x27)
-#define ADCSRA *((u8*) 0x26)
-#define ADCH *((u8*) 0x25)
-#define ADCL *((u8*) 0x24)
-
+#define ADMUX *((volatile u8*) 0x27)
+#define ADCSRA *((volatile u8*) 0x26)
+#define ADCH *((volatile u8*) 0x25)
+#define ADCL *((volatile u8*) 0x24)
 
 #endif
