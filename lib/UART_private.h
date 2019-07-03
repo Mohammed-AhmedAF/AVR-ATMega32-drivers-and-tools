91202087eae79a3c14c8bbb5f35d25d15adc86e3
Mohammed Ahmed <mohamedstarwars@gmail.com>
2019-07-03 10:58:41 +0200

    UART: Adding volatile to register definitions

--- lib/UART_private.h
+++ lib/UART_private.h
@@ -1,9 +1,9 @@
 #ifndef _UART_PRIVATE_H
 #define _UART_PRIVATE_H
 
-#define UCSRB * ((u8*) 0x2A)
-#define UCSRA * ((u8*) 0x2B)
-#define UBRRL * ((u8*) 0x29)
-#define UDR * ((u8*)) 0x2C)
+#define UCSRB * ((volatile u8*) 0x2A)
+#define UCSRA * ((volatile u8*) 0x2B)
+#define UBRRL * ((volatile u8*) 0x29)
+#define UDR * ((volatile u8*)) 0x2C)
 
 #endif
