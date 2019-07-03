aeb0ccc0e839a6d6b8d0034a517c2b410e1b818b
Mohammed Ahmed <mohamedstarwars@gmail.com>
2019-07-03 11:05:32 +0200

    DIO: Adding volatile to register definitions

--- lib/DIO_private.h
+++ lib/DIO_private.h
@@ -2,32 +2,32 @@
 #define _DIO_PRIVATE_H
 
 /*Register definitions*/
-#define DDRA *((u8*) 0x3A)
+#define DDRA *((volatile u8*) 0x3A)
 #define PORTA *((volatile u8*) 0x3B)
-#define PINA *((u8*) 0x39)
+#define PINA *((volatile u8*) 0x39)
 
-#define DDRB *((u8*) 0x37)
+#define DDRB *((volatile u8*) 0x37)
 #define PORTB *((volatile u8*) 0x38)
-#define PINB *((u8*) 0x36)
+#define PINB *((volatile u8*) 0x36)
 
-#define DDRC *((u8*) 0x34) 
+#define DDRC *((volatile u8*) 0x34) 
 #define PORTC *((volatile u8*) 0x35)
-#define PINC *((u8*) 0x33)
+#define PINC *((volatile u8*) 0x33)
 
-#define DDRD *((u8*) 0x31)
+#define DDRD *((volatile u8*) 0x31)
 #define PORTD *((volatile u8*) 0x32)
-#define PIND *((u8*) 0x30)
+#define PIND *((volatile u8*) 0x30)
 
-#define DDRE *((u8*) 0x22)
+#define DDRE *((volatile u8*) 0x22)
 #define PORTE *((volatile u8*) 0x23)
-#define PINE *((u8*) 0x21)
+#define PINE *((volatile u8*) 0x21)
 
-#define DDRF *((u8*) 0x61)
+#define DDRF *((volatile u8*) 0x61)
 #define PORTF *((volatile u8*) 0x62)
-#define PINF *((u8*) 0x20)
+#define PINF *((volatile u8*) 0x20)
 
-#define DDRG *((u8*) 0x64)
+#define DDRG *((volatile u8*) 0x64)
 #define PORTG *((volatile u8*) 0x65)
-#define PING *((u8*) 0x63)
+#define PING *((volatile u8*) 0x63)
 
 #endif
