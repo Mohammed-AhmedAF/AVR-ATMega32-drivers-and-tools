9edca19e3ab0de317bf3661b5087cf11a8829e81
Mohammed Ahmed <mohamedstarwars@gmail.com>
2019-07-03 10:52:16 +0200

    TIMER1: Adding volatile to register definitions

--- lib/TIMER1_private.h
+++ lib/TIMER1_private.h
@@ -2,14 +2,14 @@
 #define _TIMER1_PRIVATE_H
 
 /*Register definitions*/
-#define TCCR1A *((u8*) 0x4F)
-#define TCCR1B *((u8*) 0x4E)
-#define OCR1AH *((u8*) 0x4A)
-#define OCR1AL *((u8*) 0x4B)
-#define OCR1A * ((u8*) 0x4B)
-#define OCR1BH *((u8*) 0x49)
-#define OCR1BL  *((u8*) 0x48)
-#define OCR1B *((u8*) 0x48)
+#define TCCR1A *((volatile u8*) 0x4F)
+#define TCCR1B *((volatile u8*) 0x4E)
+#define OCR1AH *((volatile u8*) 0x4A)
+#define OCR1AL *((volatile u8*) 0x4B)
+#define OCR1A * ((volatile u8*) 0x4B)
+#define OCR1BH *((volatile u8*) 0x49)
+#define OCR1BL  *((volatile u8*) 0x48)
+#define OCR1B *((volatile u8*) 0x48)
 
 
 #endif
