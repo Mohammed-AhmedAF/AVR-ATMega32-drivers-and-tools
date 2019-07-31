9e25cc03ab65a1e61583292e2d82b372f085b903
Mohammed-AhmedAF <mohamedstarwars@gmail.com>
2019-07-31 09:52:35 +0200

    SPI: Adding definitions of macros for ATMEGA128 pins and ports

--- lib/SPI_interface.h
+++ lib/SPI_interface.h
@@ -13,7 +13,8 @@ u8 SPI_u8ReceiveByte(void);
 void SPI_vidEnableInterrupt(void);
 void SPI_vidDisableInterrupt(void);
 
-
+/*Definitions of pins and ports*/
+#ifdef ST_MC_ATMEGA1632
 #define SPI_MOSI_PORT DIO_PORTB
 #define SPI_MOSI_PIN DIO_PIN5
 #define SPI_MISO_PORT DIO_PORTB
@@ -22,6 +23,18 @@ void SPI_vidDisableInterrupt(void);
 #define SPI_SCK_PIN DIO_PIN7
 #define SPI_SS_PORT DIO_PORTB
 #define SPI_SS_PIN DIO_PIN4
+#endif
+
+#ifdef ST_MC_ATMEGA128
+#define SPI_MOSI_PORT DIO_PORTB
+#define SPI_MOSI_PIN DIO_PIN2
+#define SPI_MISO_PORT DIO_PORTB
+#define SPI_MISO_PIN DIO_PIN3
+#define SPI_SCK_PORT DIO_PORTB
+#define SPI_SCK_PIN DIO_PIN1
+#define SPI_SS_PORT DIO_PORTB
+#define SPI_SS_PIN DIO_PIN0
+#endif
 
 #define SPI_CLK_RISING 0
 #define SPI_CLK_FALLING 1
