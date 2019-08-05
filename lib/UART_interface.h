c3ba37c85d90af5826ae2729343ec3ddf9a441eb
Mohammed-AhmedAF <mohamedstarwars@gmail.com>
2019-08-05 13:22:33 +0200

    UART: Adding avr-io.h

--- lib/UART_interface.h
+++ lib/UART_interface.h
@@ -5,13 +5,4 @@ void UART_vidSendByte(u8);
 u8 UART_u8ReceiveByte(void);
 void UART_vidSendLine(void);
 void UART_vidSendString(u8 *);
-
-
-void UART_vidInitExtended(u8);
-void UART_vidSendByteExtended(u8,u8);
-u8 UART_u8ReceiveByteExtended(u8);
-
-#define UART_CHANNEL0 0
-#define UART_CHANNEL1 1
-
 #endif
