bb78327969f5e0c42c68a14f5b1897a2a66c23bd
Mohammed Ahmed Abd Al-Fattah <mohamedstarwars@gmail.com>
2019-11-11 14:15:10 +0200

    ADC: Adding const to register definitions

--- lib/ADC_private.h
+++ lib/ADC_private.h
@@ -1,9 +1,9 @@
 #ifndef _ADC_PRIVATE_H
 #define _ADC_PRIVATE_H
 
-#define ADMUX *((volatile u8*) 0x27)
-#define ADCSRA *((volatile u8*) 0x26)
-#define ADCH *((volatile u8*) 0x25)
-#define ADCL *((volatile u8*) 0x24)
+#define ADMUX *((volatile u8* const) 0x27)
+#define ADCSRA *((volatile u8* const) 0x26)
+#define ADCH *((volatile u8* const) 0x25)
+#define ADCL *((volatile u8* const) 0x24)
 
 #endif
