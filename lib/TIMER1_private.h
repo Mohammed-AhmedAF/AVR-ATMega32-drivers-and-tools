03a30eb307ae6dee6bd20e5dd1974ecb17d65b2b
Mohammed Ahmed Abd Al-Fattah <mohamedstarwars@gmail.com>
2019-11-11 20:09:49 +0200

    TIMER1: Add cont to register definitions

--- lib/TIMER1_private.h
+++ lib/TIMER1_private.h
@@ -2,14 +2,14 @@
 #define _TIMER1_PRIVATE_H
 
 /*Register definitions*/
-#define TCCR1A *((volatile u8*) 0x4F)
-#define TCCR1B *((volatile u8*) 0x4E)
-#define OCR1AH *((volatile u8*) 0x4A)
-#define OCR1AL *((volatile u8*) 0x4B)
-#define OCR1A * ((volatile u8*) 0x4B)
-#define OCR1BH *((volatile u8*) 0x49)
-#define OCR1BL  *((volatile u8*) 0x48)
-#define OCR1B *((volatile u8*) 0x48)
+#define TCCR1A *((volatile u8* const) 0x4F)
+#define TCCR1B *((volatile u8* const) 0x4E)
+#define OCR1AH *((volatile u8* const) 0x4A)
+#define OCR1AL *((volatile u8* const) 0x4B)
+#define OCR1A * ((volatile u8* const) 0x4B)
+#define OCR1BH *((volatile u8* const) 0x49)
+#define OCR1BL  *((volatile u8* const) 0x48)
+#define OCR1B *((volatile u8* const) 0x48)
 
 
 #endif
