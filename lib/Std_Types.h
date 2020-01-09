35eebd4d52340de7ebf65c07b50489bb819e254b
Mohammed Ahmed Abd Al-Fattah <mohamedstarwars@gmail.com>
2020-01-09 09:59:53 +0200

    STD_TYPES.h: Adding s16 definition

--- lib/Std_Types.h
+++ lib/Std_Types.h
@@ -5,6 +5,7 @@ typedef unsigned char * u8p;
 typedef unsigned char u8;
 typedef signed char s8;
 typedef unsigned short int u16;
+typedef signed short int s16;
 typedef unsigned int u32;
 typedef signed int s32;
 typedef unsigned long long u64;
