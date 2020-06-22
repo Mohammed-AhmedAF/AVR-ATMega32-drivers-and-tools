8fb4bf803df2364a7219f79b333417073ce76fcf
Mohammed Ahmed <mohamedstarwars@gmail.com>
2020-06-22 15:21:42 +0200

    Adding notification echos to script

--- tooling/beagleBone.sh
+++ tooling/beagleBone.sh
@@ -1,6 +1,15 @@
 #!/bin/env bash
 
-#
+#This script is used to enable Internet connection via USB on the BeagleBone Black (BBB)
 
-iptables --table nat --append POSTROUTING --out-interface wlp3s0 -j MASQUERADE
-iptables --append FORWARD --in-interface enx0479b787cf33 -j ACCEPT
+i=$@
+if [[ $i = "host" || $i ]]
+then
+	echo "Executing host commands..."
+	iptables --table nat --append POSTROUTING --out-interface wlp3s0 -j MASQUERADE
+	iptables --append FORWARD --in-interface enx0479b787cf33 -j ACCEPT
+elif [[ $i = "beagle" ]]
+then
+	echo "Executing Beaglebone Black commands"
+	route add default gw 192.168.7.1
+fi
