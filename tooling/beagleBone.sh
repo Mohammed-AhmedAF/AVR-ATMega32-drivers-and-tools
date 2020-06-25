f617294b1657fcc366ddbad62e547a8d6697b09d
Mohammed Ahmed <mohamedstarwars@gmail.com>
2020-06-25 18:59:53 +0200

    Adding usage note to script beaglebone.sh

--- tooling/beagleBone.sh
+++ tooling/beagleBone.sh
@@ -3,11 +3,15 @@
 #This script is used to enable Internet connection via USB on the BeagleBone Black (BBB)
 
 i=$@
-if [[ $i = "host" || $i ]]
+if [[ $# -eq 0 ]]
+then
+	echo "Usage: sudo ./beaglebone.sh host"
+elif [[ $i = "host" || $i ]]
 then
 	echo "Executing host commands..."
+	sysctl -w net.ipv4.ip_forward=1
 	iptables --table nat --append POSTROUTING --out-interface wlp3s0 -j MASQUERADE
-	iptables --append FORWARD --in-interface enx0479b787cf33 -j ACCEPT
+	iptables --append FORWARD --in-interface enp0s20f0u1 -j ACCEPT
 elif [[ $i = "beagle" ]]
 then
 	echo "Executing Beaglebone Black commands"
