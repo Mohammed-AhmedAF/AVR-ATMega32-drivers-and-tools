7b0aafc4fe00007dd01aa37ca398a6b0a6f9b1f7
Mohammed Ahmed <mohamedstarwars@gmail.com>
2020-07-03 22:12:53 +0200

    SYSCNTRL: Completing driver

--- lib/SYSCNTRL_program.c
+++ lib/SYSCNTRL_program.c
@@ -1,11 +1,3 @@
-/*
- *
- *	To control sleep mode, and other system wide properties.
- *
- *
- */
-
-
 #include "Macros.h"
 #include "Std_Types.h"
 #include "SYSCNTRL_private.h"
@@ -16,15 +8,37 @@ void SYSCNTRL_vidEnableSleep(u8 u8Mode)
 	switch(u8Mode)
 	{
 		case SYSCNTRL_SLEEPMODE_IDLE:
+		SET_BIT(MCUCR,SE);
 		CLEAR_BIT(MCUCR,SM0);
 		CLEAR_BIT(MCUCR,SM1);
 		CLEAR_BIT(MCUCR,SM2);
 		break;
 		case SYSCNTRL_SLEEPMODE_PWDOWN:
+		SET_BIT(MCUCR,SE);
 		CLEAR_BIT(MCUCR,SM0);
 		SET_BIT(MCUCR,SM1);
 		CLEAR_BIT(MCUCR,SM2);
 		break;
+		case SYSCNTRL_SLEEPMODE_PWSAVE:
+		SET_BIT(MCUCR,SE);
+		SET_BIT(MCUCR,SM0);
+		SET_BIT(MCUCR,SM1);
+		CLEAR_BIT(MCUCR,SM2);
+		break;
+		case SYSCNTRL_SLEEPMODE_STANDBY:
+		SET_BIT(MCUCR,SE);
+		CLEAR_BIT(MCUCR,SM0);
+		SET_BIT(MCUCR,SM1);
+		SET_BIT(MCUCR,SM2);
+		break;
+		case SYSCNTRL_SLEEPMODE_ESTANDBY:
+		SET_BIT(MCUCR,SE);
+		SET_BIT(MCUCR,SM0);
+		SET_BIT(MCUCR,SM1);
+		SET_BIT(MCUCR,SM2);
+		break;
 
 	}
+
+	asm("SLEEP");
 }
